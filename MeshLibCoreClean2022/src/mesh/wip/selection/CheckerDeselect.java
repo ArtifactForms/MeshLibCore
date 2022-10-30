0689cca2226babf29450d79c59ef5bf6a2246500
Simon Dietz <h.s.dietz@web.de>
2022-10-30 14:31:05 +0100

    Implemented checker deselect.

---
+++ MeshLibCoreClean2022/src/mesh/wip/selection/CheckerDeselect.java
@@ -0,0 +1,52 @@
+package mesh.wip.selection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import mesh.Face3D;
+import mesh.util.TraverseHelper;
+import mesh.wip.FaceSelection;
+
+public class CheckerDeselect {
+
+	private FaceSelection selection;
+	private ArrayList<Face3D> faces;
+	private TraverseHelper helper;
+	private HashSet<Face3D> processed;
+	
+	public CheckerDeselect(FaceSelection selection) {
+		this.selection = selection;
+		this.faces = new ArrayList<Face3D>();
+		this.faces.addAll(selection.getFaces());
+		this.helper = new TraverseHelper(selection.getMesh());
+		this.processed = new HashSet<Face3D>();
+	}
+	
+	public void deselect() {
+		if (faces.isEmpty())
+			return;
+		Face3D start = faces.get(0);
+		select(start, false);
+		selection.clear();
+		selection.addAll(faces);
+	}
+	
+	public void select(Face3D face, boolean select) {
+		if (processed.contains(face))
+			return;
+		
+		processed.add(face);
+
+		if (select) {
+			faces.remove(face);
+		}
+		
+		for (int i = 0; i < face.indices.length; i++) {
+			int from = face.indices[i];
+			int to = face.indices[(i + 1) % face.indices.length];
+			Face3D pair = helper.getFaceByEdge(to, from);
+			select(pair, !select);
+		}
+	}
+	
+}
