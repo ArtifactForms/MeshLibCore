a0a08a9983aa8a8296bf198a6ff35bfd5ceb8a3a
Simon Dietz <h.s.dietz@web.de>
2022-11-01 17:39:44 +0100

    Legacy code.

---
+++ MeshLibCoreClean2022/src/mesh/creator/FillType.java
@@ -0,0 +1,7 @@
+package mesh.creator;
+
+public enum FillType {
+
+	TRIANGLE_FAN, N_GON, NOTHING
+	
+}
---
+++ MeshLibCoreClean2022/src/mesh/creator/IMeshCreator.java
@@ -0,0 +1,9 @@
+package mesh.creator;
+
+import mesh.Mesh3D;
+
+public interface IMeshCreator {
+
+	public Mesh3D create();
+	
+}
