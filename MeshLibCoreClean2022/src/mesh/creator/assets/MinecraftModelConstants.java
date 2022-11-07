0ae9dc8ece04d2746b887518bbc35091f79f514e
Simon Dietz <h.s.dietz@web.de>
2022-11-07 20:34:16 +0100

    Legacy code.

---
+++ MeshLibCoreClean2022/src/mesh/creator/assets/MinecraftModelConstants.java
@@ -0,0 +1,19 @@
+package mesh.creator.assets;
+
+public interface MinecraftModelConstants {
+
+	public static final float UNIT = 1.0f / 16.0f;
+	
+	public static final float STEVE_MODEL_HEAD_WIDTH = 8 * UNIT;
+	
+	public static final float STEVE_MODEL_HEAD_HEIGHT = 8 * UNIT;
+	
+	public static final float STEVE_MODEL_HEAD_DEPTH = 8 * UNIT;
+	
+	public static final float STEVE_MODEL_TORSO_HEIGHT = 12 * UNIT;
+	
+	public static final float STEVE_MODEL_TORSO_WIDTH = 8 * UNIT;
+	
+	public static final float STEVE_MODEL_TORSO_DEPTH = 4 * UNIT;
+	
+}
