7f39697901d6c8db1362eabf43ed781dfec72400
Simon Dietz <h.s.dietz@web.de>
2022-11-07 06:31:32 +0100

    Moved from examples repository.

---
+++ MeshLibCoreClean2022/src/mesh/modifier/ExtrudeModifier.java
@@ -0,0 +1,47 @@
+package mesh.modifier;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import mesh.Face3D;
+import mesh.Mesh3D;
+import mesh.util.Mesh3DUtil;
+
+public class ExtrudeModifier implements IMeshModifier {
+	
+	private float scale;
+	private float amount;
+
+	public ExtrudeModifier(float scale, float amount) {
+		this.scale = scale;
+		this.amount = amount;
+	}
+	
+	public Mesh3D modify(Mesh3D mesh) {
+		modify(mesh, new ArrayList<>(mesh.faces));
+		return mesh;
+	}
+
+	public void modify(Mesh3D mesh, Collection<Face3D> faces) {
+		for (Face3D face : faces) {
+			Mesh3DUtil.extrudeFace(mesh, face, scale, amount);
+		}
+	}
+
+	public float getScale() {
+		return scale;
+	}
+
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+}
---
+++ MeshLibCoreClean2022/src/mesh/modifier/InsetModifier.java
@@ -0,0 +1,37 @@
+package mesh.modifier;
+
+import java.util.Collection;
+
+import mesh.Face3D;
+import mesh.Mesh3D;
+import mesh.util.Mesh3DUtil;
+
+public class InsetModifier implements IMeshModifier {
+	
+	private float inset;
+	
+	public InsetModifier(float inset) {
+		this.inset = inset;
+	}
+	
+	@Override
+	public Mesh3D modify(Mesh3D mesh) {
+		modify(mesh, mesh.getFaces());
+		return mesh;
+	}
+
+	public void modify(Mesh3D mesh, Collection<Face3D> faces) {
+		for (Face3D face : faces) {
+			Mesh3DUtil.insetFace(mesh, face, inset);
+		}
+	}
+
+	public float getInset() {
+		return inset;
+	}
+
+	public void setInset(float inset) {
+		this.inset = inset;
+	}
+
+}
