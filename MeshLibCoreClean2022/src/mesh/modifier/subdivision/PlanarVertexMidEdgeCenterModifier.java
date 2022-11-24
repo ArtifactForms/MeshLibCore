afe91e0da4b7016f89a51f5acb21ea4a57691e2f
Simon Dietz <h.s.dietz@web.de>
2022-11-24 16:44:29 +0100

    Refactoring. Extracted methods.

--- MeshLibCoreClean2022/src/mesh/modifier/subdivision/PlanarVertexMidEdgeCenterModifier.java
+++ MeshLibCoreClean2022/src/mesh/modifier/subdivision/PlanarVertexMidEdgeCenterModifier.java
@@ -14,42 +14,70 @@ import mesh.modifier.IMeshModifier;
 public class PlanarVertexMidEdgeCenterModifier implements IMeshModifier {
 
 	private int nextIndex;
+	private Mesh3D mesh;
+	private List<Face3D> toAdd;
 	private HashMap<Edge3D, Integer> edgeToEdgePointIndexMap;
 	
 	@Override
 	public Mesh3D modify(Mesh3D mesh) {
-		initializeEdgePointMap();
+		setMesh(mesh);
 		nextIndex = mesh.getVertexCount();
-		List<Face3D> toAdd = new ArrayList<Face3D>();
-		
-		for (Face3D face : mesh.getFaces()) {
-			int centerIndex = nextIndex;
-			Vector3f center = mesh.calculateFaceCenter(face);
-			mesh.add(center);
-			nextIndex++;
-			for (int i = 0; i < face.indices.length; i++) {
-				int fromIndex = face.indices[i];
-				int toIndex = face.indices[(i + 1) % face.indices.length];
-				Vector3f from = mesh.vertices.get(fromIndex);
-				Vector3f to = mesh.vertices.get(toIndex);
-				int edgePointIndex = getEdgePointIndex(fromIndex, toIndex);
-				Vector3f edgePoint;
-				if (edgePointIndex < 0) {
-					edgePoint = GeometryUtil.getMidpoint(from, to);
-					mesh.add(edgePoint);
-					map(fromIndex, toIndex, nextIndex);
-					edgePointIndex = nextIndex;
-					nextIndex++;;
-				}
-				toAdd.add(new Face3D(centerIndex, fromIndex, edgePointIndex));
-				toAdd.add(new Face3D(centerIndex, edgePointIndex, toIndex));
-			}
-		}
-		
-		mesh.faces.clear();
-		mesh.faces.addAll(toAdd);
-		
+		initializeEdgePointMap();
+		initializeFaceList();
+		createNewFaces();
+		clearOldFaces();
+		addNewFaces();
 		return mesh;
+	}
+	
+	private void createNewFaces() {
+		for (Face3D face : mesh.getFaces()) {
+			processFace(face);
+		}
+	}
+	
+	private void processFace(Face3D face) {
+		int centerIndex = nextIndex;
+		Vector3f center = calculateFaceCenter(face);
+		mesh.add(center);
+		nextIndex++;
+		for (int i = 0; i < face.indices.length; i++) {
+			int fromIndex = face.indices[i];
+			int toIndex = face.indices[(i + 1) % face.indices.length];
+			Vector3f from = mesh.vertices.get(fromIndex);
+			Vector3f to = mesh.vertices.get(toIndex);
+			int edgePointIndex = getEdgePointIndex(fromIndex, toIndex);
+			Vector3f edgePoint;
+			if (edgePointIndex < 0) {
+				edgePoint = calculateEdgePoint(from, to);
+				mesh.add(edgePoint);
+				map(fromIndex, toIndex, nextIndex);
+				edgePointIndex = nextIndex;
+				nextIndex++;;
+			}
+			addTriangle(centerIndex, fromIndex, edgePointIndex);
+			addTriangle(centerIndex, edgePointIndex, toIndex);
+		}
+	}
+	
+	private Vector3f calculateEdgePoint(Vector3f from, Vector3f to) {
+		return GeometryUtil.getMidpoint(from, to);
+	}
+	
+	private Vector3f calculateFaceCenter(Face3D face)  {
+		return mesh.calculateFaceCenter(face);
+	}
+	
+	private void addTriangle(int indexA, int indexB, int indexC) {
+		toAdd.add(new Face3D(indexA, indexB, indexC));
+	}
+	
+	private void clearOldFaces() {
+		mesh.faces.clear();
+	}
+	
+	private void addNewFaces() {
+		mesh.faces.addAll(toAdd);
 	}
 	
 	private int getEdgePointIndex(int fromIndex, int toIndex) {
@@ -71,6 +99,14 @@ public class PlanarVertexMidEdgeCenterModifier implements IMeshModifier {
 	
 	private void initializeEdgePointMap() {
 		edgeToEdgePointIndexMap = new HashMap<Edge3D, Integer>();
+	}
+	
+	private void initializeFaceList() {
+		toAdd = new ArrayList<Face3D>();
+	}
+	
+	private void setMesh(Mesh3D mesh) {
+		this.mesh = mesh;
 	}
 
 }
