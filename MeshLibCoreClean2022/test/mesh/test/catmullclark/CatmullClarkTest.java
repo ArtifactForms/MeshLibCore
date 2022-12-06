870a1f57bb781fb7de3d09a93362e7d6b08d6378
Simon Dietz <h.s.dietz@web.de>
2022-12-06 13:33:35 +0100

    Added tests for more coverage. Limited iterations to test to speed up things a bit more.

--- MeshLibCoreClean2022/test/mesh/test/catmullclark/CatmullClarkTest.java
+++ MeshLibCoreClean2022/test/mesh/test/catmullclark/CatmullClarkTest.java
@@ -14,7 +14,7 @@ import mesh.modifier.subdivision.CatmullClarkModifier;
 
 public class CatmullClarkTest {
 	
-	private static final int ITERATIONS_TO_TEST = 9;
+	private static final int ITERATIONS_TO_TEST = 8;
 	
 	private Mesh3D loadReference(int iteration) {
 		String file = "Catmull_Clark_Reference_Level_" + iteration + ".obj";
@@ -23,34 +23,88 @@ public class CatmullClarkTest {
 		return expectedMesh;
 	}
 	
+	private void facesAreEqual(Mesh3D expected, Mesh3D actual) {
+		for (int j = 0; j < expected.getFaceCount(); j++) {
+			Face3D expectedFace = expected.getFaceAt(j);
+			Face3D actualFace = actual.getFaceAt(j);
+			for (int index = 0; index < expectedFace.indices.length; index++) {
+				Assert.assertEquals(expectedFace.indices[index], actualFace.indices[index]);
+			}
+		}
+	}
+		
+	private void verticesAreEqual(Mesh3D expected, Mesh3D actual) {
+		for (int j = 0; j < expected.getVertexCount(); j++) {
+			Vector3f expectedVertex = expected.getVertexAt(j);
+			Vector3f actualVertex = actual.getVertexAt(j);
+			Assert.assertEquals(expectedVertex, actualVertex);
+		}
+	}
+	
+	@Test
+	public void setIterationCountToSpecifiedValueTestFaces() {
+		int iterations = 4;
+		Mesh3D expected = loadReference(iterations);
+		Mesh3D actual = new CubeCreator().create();
+		CatmullClarkModifier modifier = new CatmullClarkModifier();
+		modifier.setSubdivisions(iterations);
+		modifier.modify(actual);
+		facesAreEqual(expected, actual);
+	}
+	
+	@Test
+	public void setIterationCountToSpecifiedValueTestVertices() {
+		int iterations = 5;
+		Mesh3D expected = loadReference(iterations);
+		Mesh3D actual = new CubeCreator().create();
+		CatmullClarkModifier modifier = new CatmullClarkModifier();
+		modifier.setSubdivisions(iterations);
+		modifier.modify(actual);
+		verticesAreEqual(expected, actual);
+	}
+	
+	@Test
+	public void defaultSettingsOneIterationTestFaces() {
+		Mesh3D expected = loadReference(1);
+		Mesh3D actual = new CubeCreator().create();
+		new CatmullClarkModifier().modify(actual);
+		facesAreEqual(expected, actual);
+	}
+	
+	@Test
+	public void defaultSettingsOneIterationTestVertices() {
+		Mesh3D expected = loadReference(1);
+		Mesh3D actual = new CubeCreator().create();
+		new CatmullClarkModifier().modify(actual);
+		verticesAreEqual(expected, actual);
+	}
+	
 	@Test
 	public void createdVerticesAreEqualToThoseOfReferenceFile() {
 		for (int i = 0; i < ITERATIONS_TO_TEST; i++) {
-			Mesh3D expectedMesh = loadReference(i);
-			Mesh3D actualMesh = new CubeCreator().create();
-			new CatmullClarkModifier(i).modify(actualMesh);
-			for (int j = 0; j < expectedMesh.getVertexCount(); j++) {
-				Vector3f expectedVertex = expectedMesh.getVertexAt(j);
-				Vector3f toCompare = actualMesh.getVertexAt(j);
-				Assert.assertEquals(expectedVertex, toCompare);
-			}
+			Mesh3D expected = loadReference(i);
+			Mesh3D actual = new CubeCreator().create();
+			new CatmullClarkModifier(i).modify(actual);
+			verticesAreEqual(expected, actual);
 		}
 	}
 	
 	@Test
 	public void createdFacesAreEqualToThoseOfReferenceFile() {
 		for (int i = 0; i < ITERATIONS_TO_TEST; i++) {
-			Mesh3D expectedMesh = loadReference(i);
-			Mesh3D actualMesh = new CubeCreator().create();
-			new CatmullClarkModifier(i).modify(actualMesh);
-			for (int j = 0; j < expectedMesh.getFaceCount(); j++) {
-				Face3D expectedFace = expectedMesh.getFaceAt(j);
-				Face3D actualFace = actualMesh.getFaceAt(j);
-				for (int index = 0; index < expectedFace.indices.length; index++) {
-					Assert.assertEquals(expectedFace.indices[index], actualFace.indices[index]);
-				}
-			}
+			Mesh3D expected = loadReference(i);
+			Mesh3D actual = new CubeCreator().create();
+			new CatmullClarkModifier(i).modify(actual);
+			facesAreEqual(expected, actual);
 		}
+	}
+	
+	@Test
+	public void getSetSubdivisions() {
+		int iterations = (int) (Math.random() * Integer.MAX_VALUE);
+		CatmullClarkModifier modifier = new CatmullClarkModifier();
+		modifier.setSubdivisions(iterations);
+		Assert.assertEquals(iterations, modifier.getSubdivisions());
 	}
 
 }
