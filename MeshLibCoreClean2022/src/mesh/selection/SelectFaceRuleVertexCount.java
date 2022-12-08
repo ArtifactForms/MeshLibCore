a918d17bc2db04a49353d49cf3b41dd7f5cca66b
Simon Dietz <h.s.dietz@web.de>
2022-12-08 20:48:22 +0100

    Fixed issue.

--- MeshLibCoreClean2022/src/mesh/selection/SelectFaceRuleVertexCount.java
+++ MeshLibCoreClean2022/src/mesh/selection/SelectFaceRuleVertexCount.java
@@ -6,14 +6,16 @@ import mesh.Mesh3D;
 public class SelectFaceRuleVertexCount implements IFaceSelectionRule {
 	
 	private int vertexCount;
+	private CompareType compare;
 
 	public SelectFaceRuleVertexCount(int vertexCount, CompareType compare) {
 		this.vertexCount = vertexCount;
+		this.compare = compare;
 	}
 
 	@Override
 	public boolean isValid(Mesh3D mesh, Face3D face) {
-		return face.indices.length == vertexCount;
+		return Compare.compare(compare, face.indices.length, vertexCount);
 	}
 
 }
