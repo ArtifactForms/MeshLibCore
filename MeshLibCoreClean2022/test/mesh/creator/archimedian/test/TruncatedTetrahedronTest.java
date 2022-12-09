7185a26b6baacabcca2ae7c9d04b5c2dd6e081bd
Simon Dietz <h.s.dietz@web.de>
2022-12-09 15:29:07 +0100

    Added test.

---
+++ MeshLibCoreClean2022/test/mesh/creator/archimedian/test/TruncatedTetrahedronTest.java
@@ -0,0 +1,64 @@
+package mesh.creator.archimedian.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import math.Mathf;
+import mesh.Mesh3D;
+import mesh.creator.archimedian.TruncatedTetrahedronCreator;
+import mesh.util.Geometry;
+import util.MeshTest;
+
+public class TruncatedTetrahedronTest {
+	
+	private Mesh3D mesh;
+	
+	@Before
+	public void setUp() {
+		mesh = new TruncatedTetrahedronCreator().create();
+	}
+	
+	@Test
+	public void hasTwelveVertices() {
+		Assert.assertEquals(12, mesh.getVertexCount());
+	}
+	
+	@Test
+	public void hasEightFaces() {
+		Assert.assertEquals(8, mesh.getFaceCount());
+	}
+	
+	@Test
+	public void hasFourTriangularFaces() {
+		MeshTest.assertTriangleCountEquals(mesh, 4);
+	}
+	
+	@Test
+	public void hasFourHexagonFaces() {
+		MeshTest.assertHexagonCountEquals(mesh, 4);
+	}
+	
+	@Test
+	public void hasEighteenEdges() {
+		MeshTest.assertEdgeCountEquals(mesh, 18);
+	}
+	
+	@Test
+	public void isManifold() {
+		MeshTest.assertIsManifold(mesh);
+	}
+	
+	@Test
+	public void fulfillsEulerCharacteristic() {
+		MeshTest.assertFulfillsEulerCharacteristic(mesh);
+	}
+	
+	@Test
+	public void everyEdgeHasLengthOfSqrtOfEight() {
+		float delta = 0;
+		float expcted = Mathf.sqrt(8);
+		MeshTest.assertEveryEgdgeHasALengthOf(mesh, expcted, delta);
+	}
+
+}
