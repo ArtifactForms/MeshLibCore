3b8df558e6b82f336612236a3973c3e563a2b97d
Simon Dietz <h.s.dietz@web.de>
2022-12-09 15:31:16 +0100

    Fixed imports.

--- MeshLibCoreClean2022/test/mesh/creator/archimedian/test/TruncatedTetrahedronTest.java
+++ MeshLibCoreClean2022/test/mesh/creator/archimedian/test/TruncatedTetrahedronTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import math.Mathf;
 import mesh.Mesh3D;
 import mesh.creator.archimedian.TruncatedTetrahedronCreator;
-import mesh.util.Geometry;
 import util.MeshTest;
 
 public class TruncatedTetrahedronTest {
