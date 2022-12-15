e47fe445c06075d7a07799fdc1a220574c4efa53
Simon Dietz <h.s.dietz@web.de>
2022-12-15 22:16:31 +0100

    Format changed.

--- MeshLibCoreClean2022/src/mesh/creator/primitives/PlaneCreator.java
+++ MeshLibCoreClean2022/src/mesh/creator/primitives/PlaneCreator.java
@@ -7,17 +7,17 @@ import mesh.creator.IMeshCreator;
 public class PlaneCreator implements IMeshCreator {
 
 	private float radius;
-	
+
 	private Mesh3D mesh;
-	
+
 	public PlaneCreator() {
 		this(1);
 	}
-	
+
 	public PlaneCreator(float size) {
 		this.radius = size;
 	}
-	
+
 	@Override
 	public Mesh3D create() {
 		initializeMesh();
@@ -25,26 +25,26 @@ public class PlaneCreator implements IMeshCreator {
 		createFace();
 		return mesh;
 	}
-	
+
 	private void initializeMesh() {
 		mesh = new Mesh3D();
 	}
-	
+
 	private void createVertices() {
 		addVertex(+radius, 0, -radius);
 		addVertex(+radius, 0, +radius);
 		addVertex(-radius, 0, +radius);
 		addVertex(-radius, 0, -radius);
 	}
-	
+
 	private void addVertex(float x, float y, float z) {
 		mesh.addVertex(x, y, z);
 	}
-	
+
 	private void createFace() {
 		mesh.add(new Face3D(0, 1, 2, 3));
 	}
-	
+
 	public float getRadius() {
 		return radius;
 	}
@@ -52,11 +52,11 @@ public class PlaneCreator implements IMeshCreator {
 	public void setRadius(float radius) {
 		this.radius = radius;
 	}
-	
+
 	public float getSize() {
 		return radius * 2;
 	}
-	
+
 	public void setSize(float size) {
 		setRadius(size * 0.5f);
 	}
