9ddf685d72d152243befc3b91bd173a2ee7adea4
Simon Dietz <h.s.dietz@web.de>
2022-12-15 13:34:09 +0100

    Extracted methods.

--- MeshLibCoreClean2022/src/mesh/creator/platonic/OctahedronCreator.java
+++ MeshLibCoreClean2022/src/mesh/creator/platonic/OctahedronCreator.java
@@ -16,34 +16,46 @@ public class OctahedronCreator implements IMeshCreator {
 		this.radius = radius;
 	}
 	
-	private void createVertices() {
-		mesh.addVertex(0, 0, radius);
-		mesh.addVertex(0, 0, -radius);
-		mesh.addVertex(radius, 0, 0);
-		mesh.addVertex(-radius, 0, 0);
-		mesh.addVertex(0, radius, 0);
-		mesh.addVertex(0, -radius, 0);
-	}
-	
-	private void createFaces() {
-		mesh.addFace(2, 5, 1);	
-		mesh.addFace(1, 5, 3);
-		mesh.addFace(3, 5, 0);
-		mesh.addFace(0, 5, 2);
-		mesh.addFace(2, 1, 4);
-		mesh.addFace(1, 3, 4);
-		mesh.addFace(3, 0, 4);
-		mesh.addFace(0, 2, 4);
-	}
-	
 	@Override
 	public Mesh3D create() {
-		mesh = new Mesh3D();
+		initializeMesh();
 		createVertices();
 		createFaces();
 		return mesh;
 	}
-
+	
+	private void initializeMesh() {
+		mesh = new Mesh3D();
+	}
+	
+	private void createVertices() {
+		addVertex(0, 0, radius);
+		addVertex(0, 0, -radius);
+		addVertex(radius, 0, 0);
+		addVertex(-radius, 0, 0);
+		addVertex(0, radius, 0);
+		addVertex(0, -radius, 0);
+	}
+	
+	private void createFaces() {
+		addFace(2, 5, 1);	
+		addFace(1, 5, 3);
+		addFace(3, 5, 0);
+		addFace(0, 5, 2);
+		addFace(2, 1, 4);
+		addFace(1, 3, 4);
+		addFace(3, 0, 4);
+		addFace(0, 2, 4);
+	}
+	
+	private void addVertex(float x, float y, float z) {
+		mesh.addVertex(x, y, z);
+	}
+	
+	private void addFace(int... indices) {
+		mesh.addFace(indices);
+	}
+	
 	public float getRadius() {
 		return radius;
 	}
