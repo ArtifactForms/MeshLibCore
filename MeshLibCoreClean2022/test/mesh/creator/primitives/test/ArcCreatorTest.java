a9544aa04eab090173cf4d23d9f6b5d8d0235405
Simon Dietz <h.s.dietz@web.de>
2022-12-27 10:46:07 +0100

    Added tests.

---
+++ MeshLibCoreClean2022/test/mesh/creator/primitives/test/ArcCreatorTest.java
@@ -0,0 +1,66 @@
+package mesh.creator.primitives.test;
+
+import java.util.HashSet;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import math.Vector3f;
+import mesh.Mesh3D;
+import mesh.creator.primitives.ArcCreator;
+
+public class ArcCreatorTest {
+
+    @Test
+    public void createsMeshWithThirtyTwoVerticesByDefault() {
+	ArcCreator creator = new ArcCreator();
+	Mesh3D mesh = creator.create();
+	Assert.assertEquals(32, mesh.getVertexCount());
+    }
+
+    @Test
+    public void createsMeshWithZeroFacesByDefault() {
+	ArcCreator creator = new ArcCreator();
+	Mesh3D mesh = creator.create();
+	Assert.assertEquals(0, mesh.getFaceCount());
+    }
+
+    @Test
+    public void distanceFromOriginToEachCreatedVertexIsEqualToOneByDefault() {
+	Vector3f origin = new Vector3f(0, 0, 0);
+	ArcCreator creator = new ArcCreator();
+	Mesh3D mesh = creator.create();
+	for (Vector3f v : mesh.getVertices())
+	    Assert.assertEquals(1, origin.distance(v), 0.0000001f);
+    }
+
+    @Test
+    public void distanceFromOriginToEachCreatedVertexIsEqualToRadius() {
+	Random random = new Random();
+	float randomRadius = random.nextFloat();
+	Vector3f origin = new Vector3f(0, 0, 0);
+	ArcCreator creator = new ArcCreator();
+	creator.setRadius(randomRadius);
+	Mesh3D mesh = creator.create();
+	for (Vector3f v : mesh.getVertices())
+	    Assert.assertEquals(randomRadius, origin.distance(v), 0.0000001f);
+    }
+
+    @Test
+    public void eachVertexIsDifferent() {
+	ArcCreator creator = new ArcCreator();
+	Mesh3D mesh = creator.create();
+	HashSet<Vector3f> vertices = new HashSet<Vector3f>(mesh.getVertices());
+	Assert.assertEquals(32, vertices.size());
+    }
+
+    @Test
+    public void vertices() {
+	ArcCreator creator = new ArcCreator();
+	creator.setVertices(77);
+	Mesh3D mesh = creator.create();
+	Assert.assertEquals(77, mesh.getVertexCount());
+    }
+
+}
---
+++ MeshLibCoreClean2022/test/mesh/creator/primitives/test/BoxCreatorTest.java
@@ -0,0 +1,171 @@
+package mesh.creator.primitives.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import math.Mathf;
+import math.Vector3f;
+import mesh.Face3D;
+import mesh.Mesh3D;
+import mesh.creator.primitives.BoxCreator;
+import mesh.creator.primitives.CubeCreator;
+import mesh.creator.test.manifold.ManifoldTest;
+import mesh.selection.FaceSelection;
+import mesh.util.Bounds3;
+import util.MeshTest;
+
+public class BoxCreatorTest {
+
+    @Test
+    public void createdMeshIsManifold() {
+	new ManifoldTest(new BoxCreator().create()).assertIsManifold();
+    }
+
+    @Test
+    public void createdMeshIsNotNullByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	Assert.assertNotNull(mesh);
+    }
+
+    @Test
+    public void everyEdgeHasTheLengthOfOne() {
+	float delta = 0;
+	float expectedEdgeLength = 1.0f;
+	Mesh3D mesh = new BoxCreator().create();
+	MeshTest.assertEveryEdgeHasALengthOf(mesh, expectedEdgeLength, delta);
+    }
+
+    @Test
+    public void fulfillsEulerCharacteristic() {
+	Mesh3D mesh = new BoxCreator().create();
+	MeshTest.assertFulfillsEulerCharacteristic(mesh);
+    }
+
+    @Test
+    public void faceCountIsSix() {
+	Mesh3D mesh = new BoxCreator().create();
+	Assert.assertEquals(6, mesh.getFaceCount());
+    }
+
+    @Test
+    public void vertexCountIsEight() {
+	Mesh3D mesh = new BoxCreator().create();
+	Assert.assertEquals(8, mesh.getVertexCount());
+    }
+
+    @Test
+    public void sixSquareFaces() {
+	Mesh3D mesh = new BoxCreator().create();
+	FaceSelection selection = new FaceSelection(mesh);
+	selection.selectQuads();
+	Assert.assertEquals(6, selection.size());
+    }
+
+    @Test
+    public void twelveEdges() {
+	Mesh3D mesh = new BoxCreator().create();
+	MeshTest.assertEdgeCountEquals(mesh, 12);
+    }
+
+    @Test
+    public void absValueOfEveryXCoordinateIsOPointFiveByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	for (Vector3f v : mesh.vertices)
+	    Assert.assertEquals(0.5f, Mathf.abs(v.x), 0);
+    }
+
+    @Test
+    public void absValueOfEveryYCoordinateIsOPointFiveByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	for (Vector3f v : mesh.vertices)
+	    Assert.assertEquals(0.5f, Mathf.abs(v.y), 0);
+    }
+
+    @Test
+    public void absValueOfEveryZCoordinateIsOPointFiveByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	for (Vector3f v : mesh.vertices)
+	    Assert.assertEquals(0.5f, Mathf.abs(v.z), 0);
+    }
+
+    @Test
+    public void widthIsOneByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	Bounds3 bounds = mesh.calculateBounds();
+	Assert.assertEquals(1, bounds.getWidth(), 0);
+    }
+
+    @Test
+    public void heightIsOneByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	Bounds3 bounds = mesh.calculateBounds();
+	Assert.assertEquals(1, bounds.getHeight(), 0);
+    }
+
+    @Test
+    public void depthIsOneByDefault() {
+	Mesh3D mesh = new BoxCreator().create();
+	Bounds3 bounds = mesh.calculateBounds();
+	Assert.assertEquals(1, bounds.getDepth(), 0);
+    }
+
+    @Test
+    public void getSetDepth() {
+	float depth = Mathf.random(Float.MIN_VALUE, Float.MAX_VALUE);
+	BoxCreator creator = new BoxCreator();
+	creator.setDepth(depth);
+	Assert.assertEquals(depth, creator.getDepth(), 0);
+    }
+    
+    @Test
+    public void normalsPointOutwards() {
+	Mesh3D mesh = new BoxCreator().create();
+	Vector3f center = new Vector3f();
+	for (Face3D face : mesh.getFaces()) {
+	    Vector3f faceNormal = mesh.calculateFaceNormal(face);
+	    Vector3f faceCenter = mesh.calculateFaceCenter(face);
+	    Vector3f a = faceCenter.subtract(center);
+	    float dotProduct = faceNormal.dot(a);
+	    Assert.assertTrue(dotProduct >= 0);
+	}
+    }
+    
+    @Test
+    public void defaultContainsCubeVertices() {
+	Mesh3D cube = new CubeCreator(0.5f).create();
+	Mesh3D box = new BoxCreator().create();
+	for (Vector3f v : cube.vertices)
+	    Assert.assertTrue(box.vertices.contains(v));
+    }
+    
+    @Test
+    public void boundsWidth() {
+	float expectedWidth = 1922.24f;
+	BoxCreator creator = new BoxCreator();
+	creator.setWidth(expectedWidth);
+	Mesh3D box = creator.create();
+	Bounds3 bounds = box.calculateBounds();
+	Assert.assertEquals(expectedWidth, bounds.getWidth(), 0);
+    }
+    
+    @Test
+    public void boundsHeight() {
+	float expectedHeight = 23.2421f;
+	BoxCreator creator = new BoxCreator();
+	creator.setHeight(expectedHeight);
+	Mesh3D box = creator.create();
+	Bounds3 bounds = box.calculateBounds();
+	Assert.assertEquals(expectedHeight, bounds.getHeight(), 0);
+    }
+    
+    @Test
+    public void boundsDepth() {
+	float expectedDepth = 2348.21112f;
+	BoxCreator creator = new BoxCreator();
+	creator.setDepth(expectedDepth);
+	Mesh3D box = creator.create();
+	Bounds3 bounds = box.calculateBounds();
+	Assert.assertEquals(expectedDepth, bounds.getDepth(), 0);
+    }
+
+}
