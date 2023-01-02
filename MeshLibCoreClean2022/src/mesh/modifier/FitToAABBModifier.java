94c9e4ac3114cba94ab467dc166a45045528b3e4
Simon Dietz <h.s.dietz@web.de>
2023-01-02 16:56:47 +0100

    Added test. Fixed modifier.

--- MeshLibCoreClean2022/src/mesh/modifier/FitToAABBModifier.java
+++ MeshLibCoreClean2022/src/mesh/modifier/FitToAABBModifier.java
@@ -1,5 +1,6 @@
 package mesh.modifier;
 
+import math.Mathf;
 import mesh.Mesh3D;
 import mesh.util.Bounds3;
 
@@ -8,6 +9,7 @@ public class FitToAABBModifier implements IMeshModifier {
 	private float width;
 	private float height;
 	private float depth;
+	private Mesh3D mesh;
 
 	public FitToAABBModifier(float width, float height, float depth) {
 		this.width = width;
@@ -17,12 +19,43 @@ public class FitToAABBModifier implements IMeshModifier {
 
 	@Override
 	public Mesh3D modify(Mesh3D mesh) {
-		Bounds3 bounds = mesh.calculateBounds();
-		float sx = 1f / bounds.getWidth() * width;
-		float sy = 1f / bounds.getHeight() * height;
-		float sz = 1f / bounds.getDepth() * depth;
-		mesh.scale(sx, sy, sz);
+		setMesh(mesh);
+		scale(calculateScale());
 		return mesh;
+	}
+	
+	private void scale(float scale) {
+		mesh.scale(scale, scale, scale);
+	}
+	
+	private float calculateScale() {
+		float min = minTargetValue();
+		float max = maxSourceValue();
+		return min / max;
+	}
+	
+	private float minTargetValue() {
+		return min(width, height, depth);
+	}
+	
+	private float maxSourceValue() {
+		Bounds3 bounds = mesh.calculateBounds();
+		float width = bounds.getWidth();
+		float height = bounds.getHeight();
+		float depth = bounds.getDepth();
+		return max(width, height, depth);
+	}
+	
+	private float min(float... values) {
+		return Mathf.min(values);
+	}
+	
+	private float max(float... values) {
+		return Mathf.max(values);
+	}
+	
+	private void setMesh(Mesh3D mesh) {
+		this.mesh = mesh;
 	}
 
 }
