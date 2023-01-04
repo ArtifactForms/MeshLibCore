9f9a2467064c300c0c2cb3ada457a0be9ab7ceec
Simon Dietz <h.s.dietz@web.de>
2023-01-04 21:31:14 +0100

    Extracted methods.

--- MeshLibCoreClean2022/src/mesh/creator/special/AntiprismCreator.java
+++ MeshLibCoreClean2022/src/mesh/creator/special/AntiprismCreator.java
@@ -8,6 +8,7 @@ import mesh.creator.IMeshCreator;
 public class AntiprismCreator implements IMeshCreator {
 
 	private int n;
+	private Mesh3D mesh;
 
 	public AntiprismCreator() {
 		this(6);
@@ -19,20 +20,32 @@ public class AntiprismCreator implements IMeshCreator {
 
 	@Override
 	public Mesh3D create() {
-		Mesh3D mesh = new Mesh3D();
-
-		int n2 = n + n;
-		int[] indices0 = new int[n];
-		int[] indices1 = new int[n];
-		float h = Mathf.sqrt(((Mathf.cos(Mathf.PI / n)) - (Mathf.cos(Mathf.TWO_PI / n))) / 2.0f);
-
+		initializeMesh();
+		createVertices();
+		createFaces(mesh);
+		return mesh;
+	}
+	
+	private void initializeMesh() {
+		mesh = new Mesh3D();
+	}
+	
+	private void createVertices() {
+		float a = Mathf.cos(Mathf.PI / n);
+		float b = Mathf.cos(Mathf.TWO_PI / n);
+		float h = Mathf.sqrt((a - b) / 2.0f);
 		for (int k = 0; k < 2 * n; k++) {
 			float x = Mathf.cos(k * Mathf.PI / n);
 			float z = Mathf.sin(k * Mathf.PI / n);
 			float y = Mathf.pow(-1, k) * h;
 			mesh.addVertex(x, y, z);
 		}
+	}
 
+	private void createFaces(Mesh3D mesh) {
+		int n2 = n + n;
+		int[] indices0 = new int[n];
+		int[] indices1 = new int[n];
 		for (int i = 0; i < 2 * n; i++) {
 			if (i % 2 == 1) {
 				mesh.addFace(i % n2, (i + 1) % n2, (i + 2) % n2);
@@ -42,11 +55,8 @@ public class AntiprismCreator implements IMeshCreator {
 				indices1[(n - 1) - (i / 2)] = i;
 			}
 		}
-
 		mesh.add(new Face3D(indices0));
 		mesh.add(new Face3D(indices1));
-
-		return mesh;
 	}
 
 	public int getN() {
