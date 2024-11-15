ebf8fb3124d618dc6f911008e58c9f3be50f91be
Simon Dietz <h.s.dietz@web.de>
2024-11-15 18:07:56 +0100

    Refactoring. Extracted methods.

--- MeshLibCoreClean2022/src/mesh/modifier/subdivision/QuadsToTrianglesModifier.java
+++ MeshLibCoreClean2022/src/mesh/modifier/subdivision/QuadsToTrianglesModifier.java
@@ -9,24 +9,55 @@ import mesh.modifier.IMeshModifier;
 
 public class QuadsToTrianglesModifier implements IMeshModifier {
 
+    private Mesh3D mesh;
+
+    private List<Face3D> facesToRemove;
+
+    private List<Face3D> facesToAdd;
+
     @Override
     public Mesh3D modify(Mesh3D mesh) {
-        List<Face3D> facesToRemove = new ArrayList<Face3D>();
-        List<Face3D> facesToAdd = new ArrayList<Face3D>();
-        for (Face3D face : mesh.faces) {
-            if (face.indices.length == 4) {
-                Face3D triangle0 = new Face3D(face.indices[0], face.indices[1],
-                        face.indices[2]);
-                Face3D triangle1 = new Face3D(face.indices[2], face.indices[3],
-                        face.indices[0]);
-                facesToRemove.add(face);
-                facesToAdd.add(triangle0);
-                facesToAdd.add(triangle1);
-            }
-        }
-        mesh.faces.removeAll(facesToRemove);
-        mesh.faces.addAll(facesToAdd);
+        setMesh(mesh);
+        initializeLists();
+        subdivideFaces();
+        removeOldFaces();
+        addNewFaces();
         return mesh;
+    }
+
+    private void initializeLists() {
+        facesToRemove = new ArrayList<Face3D>();
+        facesToAdd = new ArrayList<Face3D>();
+    }
+
+    private void subdivideFaces() {
+        for (Face3D face : mesh.faces)
+            subdivideFace(face);
+    }
+
+    private void subdivideFace(Face3D face) {
+        if (face.indices.length != 4)
+            return;
+
+        int[] indices = face.indices;
+        Face3D triangle0 = new Face3D(indices[0], indices[1], indices[2]);
+        Face3D triangle1 = new Face3D(indices[2], indices[3], indices[0]);
+
+        facesToRemove.add(face);
+        facesToAdd.add(triangle0);
+        facesToAdd.add(triangle1);
+    }
+
+    private void removeOldFaces() {
+        mesh.faces.removeAll(facesToRemove);
+    }
+
+    private void addNewFaces() {
+        mesh.faces.addAll(facesToAdd);
+    }
+
+    private void setMesh(Mesh3D mesh) {
+        this.mesh = mesh;
     }
 
 }
