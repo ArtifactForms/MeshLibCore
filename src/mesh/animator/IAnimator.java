ec050e34d2047789bf77245253a6ab31641a6cef
Simon Dietz <h.s.dietz@web.de>
2024-11-16 00:48:15 +0100

    changed folder structure

---
+++ src/mesh/animator/IAnimator.java
@@ -0,0 +1,19 @@
+package mesh.animator;
+
+public interface IAnimator {
+
+    void update(float tpf);
+
+    void onUpdate(float tpf);
+
+    void restore();
+
+    boolean isEnabled();
+
+    void setEnabled(boolean enabled);
+
+    boolean isFinished();
+
+    void setFinished(boolean finished);
+
+}
