55066aca2c45753b8ad782da98d4c910305fd4bd
Simon Dietz <h.s.dietz@web.de>
2024-11-16 15:18:22 +0100

    Readded tests

---
+++ src/test/java/mesh/modifier/subdivision/test/LinearSubdivisionModifierTest.java
@@ -0,0 +1,308 @@
+package mesh.modifier.subdivision.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import math.Vector3f;
+import mesh.Face3D;
+import mesh.Mesh3D;
+import mesh.creator.archimedian.CuboctahedronCreator;
+import mesh.creator.platonic.IcosahedronCreator;
+import mesh.creator.primitives.CubeCreator;
+import mesh.creator.primitives.PlaneCreator;
+import mesh.modifier.IMeshModifier;
+import mesh.modifier.subdivision.LinearSubdivisionModifier;
+import mesh.util.Bounds3;
+import util.MeshTestUtil;
+
+public class LinearSubdivisionModifierTest {
+
+    private LinearSubdivisionModifier modifier;
+
+    private Mesh3D cubeMesh;
+
+    @Before
+    public void setUp() {
+        modifier = new LinearSubdivisionModifier();
+        cubeMesh = new CubeCreator().create();
+    }
+
+    @Test
+    public void modifierImplementsModifierInterface() {
+        assertTrue(modifier instanceof IMeshModifier);
+    }
+
+    @Test
+    public void returnsReferenceToOriginalMeshInstance() {
+        Mesh3D result = modifier.modify(cubeMesh);
+        assertTrue(cubeMesh == result);
+    }
+
+    @Test
+    public void subdividedCubeHasTwentyFourFaces() {
+        modifier.modify(cubeMesh);
+        assertEquals(24, cubeMesh.getFaceCount());
+    }
+
+    @Test
+    public void twiceSubdividedCubeHasNintySixFaces() {
+        modifier.modify(cubeMesh);
+        modifier.modify(cubeMesh);
+        assertEquals(96, cubeMesh.getFaceCount());
+    }
+
+    @Test
+    public void subdividedCubeHasTwentySixVertices() {
+        modifier.modify(cubeMesh);
+        assertEquals(26, cubeMesh.getVertexCount());
+    }
+
+    @Test
+    public void twiceSubdividedCubeHasNintyEightVertices() {
+        modifier.modify(cubeMesh);
+        modifier.modify(cubeMesh);
+        assertEquals(98, cubeMesh.getVertexCount());
+    }
+
+    @Test
+    public void subdividedCubeIsManifold() {
+        modifier.modify(cubeMesh);
+        assertTrue(MeshTestUtil.isManifold(cubeMesh));
+    }
+
+    @Test
+    public void subdividedCubeHasNoLooseVertices() {
+        modifier.modify(cubeMesh);
+        assertTrue(MeshTestUtil.meshHasNoLooseVertices(cubeMesh));
+    }
+
+    @Test
+    public void twiceSubdividedCubeHasNoLooseVertices() {
+        modifier.modify(cubeMesh);
+        modifier.modify(cubeMesh);
+        assertTrue(MeshTestUtil.meshHasNoLooseVertices(cubeMesh));
+    }
+
+    @Test
+    public void subdividedCubeHasFaceNormalPointOutward() {
+        modifier.modify(cubeMesh);
+        assertTrue(MeshTestUtil.normalsPointOutwards(cubeMesh));
+    }
+
+    @Test
+    public void subdividedCubeContainsVerticesAtOriginalCubePositions() {
+        Mesh3D originalCube = new CubeCreator().create();
+        Mesh3D subdividedCube = new CubeCreator().create();
+        modifier.modify(subdividedCube);
+        for (Vector3f v : originalCube.getVertices()) {
+            assertTrue(subdividedCube.getVertices().contains(v));
+        }
+    }
+
+    @Test
+    public void subdividedCubeContainsFaceCenterVertices() {
+        Mesh3D originalCube = new CubeCreator().create();
+        Mesh3D subdividedCube = new CubeCreator().create();
+        modifier.modify(subdividedCube);
+        for (Face3D face : originalCube.getFaces()) {
+            Vector3f center = originalCube.calculateFaceCenter(face);
+            assertTrue(subdividedCube.getVertices().contains(center));
+        }
+    }
+
+    @Test
+    public void subdividedCuveHasNoDuplicatedFaces() {
+        modifier.modify(cubeMesh);
+        assertTrue(MeshTestUtil.meshHasNoDuplicatedFaces(cubeMesh));
+    }
+
+    @Test
+    public void subdividedCubeConsistsOfQuadsOnly() {
+        modifier.modify(cubeMesh);
+        int faceCount = cubeMesh.getFaceCount();
+        assertTrue(MeshTestUtil.isQuadCountEquals(cubeMesh, faceCount));
+    }
+
+    @Test
+    public void subdividedIcosahedronConsistsOfTrianglesOnly() {
+        Mesh3D mesh = new IcosahedronCreator().create();
+        modifier.modify(mesh);
+        int faceCount = mesh.getFaceCount();
+        assertTrue(MeshTestUtil.isTriangleCountEquals(mesh, faceCount));
+    }
+
+    @Test
+    public void subdividedIcosahedronHasEightyFaces() {
+        Mesh3D mesh = new IcosahedronCreator().create();
+        modifier.modify(mesh);
+        assertEquals(80, mesh.getFaceCount());
+    }
+
+    @Test
+    public void subdividedIcosahedronHasFourtyTwoVertices() {
+        Mesh3D mesh = new IcosahedronCreator().create();
+        modifier.modify(mesh);
+        assertEquals(42, mesh.getVertexCount());
+    }
+
+    @Test
+    public void subdividedIcosahedronIsManifold() {
+        Mesh3D mesh = new IcosahedronCreator().create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.isManifold(mesh));
+    }
+
+    @Test
+    public void subdividedIcosahedronHasNoLooseVertices() {
+        Mesh3D mesh = new IcosahedronCreator().create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.meshHasNoLooseVertices(mesh));
+    }
+
+    @Test
+    public void subdividedCubeIsWithinBounds() {
+        modifier.modify(cubeMesh);
+        Bounds3 bounds = cubeMesh.calculateBounds();
+        assertEquals(2, bounds.getWidth(), 0);
+        assertEquals(2, bounds.getHeight(), 0);
+        assertEquals(2, bounds.getDepth(), 0);
+    }
+
+    @Test
+    public void everyFaceContainsCenterVertexIndex() {
+        Mesh3D plane = new PlaneCreator().create();
+        Face3D face = plane.getFaceAt(0);
+        Vector3f center = plane.calculateFaceCenter(face);
+        modifier.modify(plane);
+        for (Face3D f : plane.getFaces()) {
+            boolean containsCenter = false;
+            for (int i = 0; i < f.indices.length; i++) {
+                Vector3f v = plane.getVertexAt(f.indices[i]);
+                containsCenter |= v.equals(center);
+            }
+            assertTrue(containsCenter);
+        }
+    }
+
+    @Test
+    public void removingCenterAndEdgePointsLeavesOriginalVerticesQuadCase() {
+        Mesh3D originalCube = new CubeCreator().create();
+        modifier.modify(cubeMesh);
+        List<Vector3f> vertices = cubeMesh.getVertices();
+        vertices.removeAll(calculateEdgePoints(originalCube));
+        for (Face3D face : originalCube.getFaces()) {
+            Vector3f center = originalCube.calculateFaceCenter(face);
+            vertices.remove(center);
+        }
+        assertEquals(8, vertices.size());
+        for (Vector3f v : originalCube.getVertices()) {
+            assertTrue(vertices.contains(v));
+        }
+    }
+
+    @Test
+    public void removingEdgePointsLeavesOriginalVerticesTriangleCase() {
+        Mesh3D original = new IcosahedronCreator().create();
+        Mesh3D mesh = new IcosahedronCreator().create();
+        int expectedVertexCount = original.getVertexCount();
+        modifier.modify(mesh);
+        List<Vector3f> vertices = mesh.getVertices();
+        vertices.removeAll(calculateEdgePoints(original));
+        assertEquals(expectedVertexCount, vertices.size());
+        for (Vector3f v : original.getVertices()) {
+            assertTrue(vertices.contains(v));
+        }
+    }
+
+    private List<Vector3f> calculateEdgePoints(Mesh3D mesh) {
+        List<Vector3f> edgePoints = new ArrayList<>();
+        for (Face3D face : mesh.getFaces()) {
+            int length = face.indices.length;
+            for (int i = 0; i < length; i++) {
+                Vector3f from = mesh.getVertexAt(face.indices[i]);
+                Vector3f to = mesh.getVertexAt(face.indices[(i + 1) % length]);
+                Vector3f edgePoint = from.add(to).mult(0.5f);
+                edgePoints.add(edgePoint);
+            }
+        }
+        return edgePoints;
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasThrirtyTwoTriangularFaces() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.isTriangleCountEquals(mesh, 32));
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasTwentyFourQuads() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.isQuadCountEquals(mesh, 24));
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasNoLooseVertices() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.meshHasNoLooseVertices(mesh));
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasNoDuplicatedFaces() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.meshHasNoDuplicatedFaces(mesh));
+    }
+
+    @Test
+    public void subdividedCuboctahedronIsManifold() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertTrue(MeshTestUtil.isManifold(mesh));
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasFourtyTwoVertices() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        assertEquals(42, mesh.getVertexCount());
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasNoDuplicatedVertices() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        HashSet<Vector3f> vertices = new HashSet<>();
+        vertices.addAll(mesh.getVertices());
+        assertEquals(42, mesh.getVertexCount());
+    }
+
+    @Test
+    public void subdividedCuboctahedronHasNoDuplicatedVerticesThreshold() {
+        CuboctahedronCreator creator = new CuboctahedronCreator();
+        Mesh3D mesh = creator.create();
+        modifier.modify(mesh);
+        for (Vector3f v : mesh.vertices)
+            v.roundLocalDecimalPlaces(2);
+        HashSet<Vector3f> vertices = new HashSet<>();
+        vertices.addAll(mesh.getVertices());
+        assertEquals(42, mesh.getVertexCount());
+    }
+
+}
---
+++ src/test/java/mesh/modifier/subdivision/test/PlanarVertexCenterCubeTest.java
@@ -0,0 +1,205 @@
+package mesh.modifier.subdivision.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import math.Vector3f;
+import mesh.Face3D;
+import mesh.Mesh3D;
+import mesh.creator.primitives.CubeCreator;
+import mesh.modifier.subdivision.PlanarVertexCenterModifier;
+import mesh.selection.FaceSelection;
+import util.MeshTestUtil;
+
+public class PlanarVertexCenterCubeTest {
+
+    private Mesh3D cube;
+
+    private PlanarVertexCenterModifier modifier;
+
+    @Before
+    public void setUp() {
+        cube = new CubeCreator().create();
+        modifier = new PlanarVertexCenterModifier();
+        modifier.modify(cube);
+    }
+
+    @Test
+    public void resultMeshHasFourteenVertices() {
+        int expectedVertexCount = 14;
+        assertEquals(expectedVertexCount, cube.getVertexCount());
+        assertEquals(expectedVertexCount, cube.vertices.size());
+        assertEquals(expectedVertexCount, cube.getVertices().size());
+        assertEquals(expectedVertexCount,
+                cube.getVertices(0, cube.getVertexCount()).size());
+    }
+
+    @Test
+    public void resultMeshHasTwentyFourFaces() {
+        int expectedFaceCount = 24;
+        assertEquals(expectedFaceCount, cube.getFaceCount());
+        assertEquals(expectedFaceCount, cube.faces.size());
+        assertEquals(expectedFaceCount, cube.getFaces().size());
+        assertEquals(expectedFaceCount,
+                cube.getFaces(0, cube.getFaceCount()).size());
+    }
+
+    @Test
+    public void resultMeshHasTwentyFourTriangularFaces() {
+        assertTrue(MeshTestUtil.isTriangleCountEquals(cube, 24));
+    }
+
+    @Test
+    public void resultMeshHasThirtySixEdges() {
+        MeshTestUtil.assertEdgeCountEquals(cube, 36);
+    }
+
+    @Test
+    public void resultMeshHasNoLooseVertices() {
+        assertTrue(MeshTestUtil.meshHasNoLooseVertices(cube));
+    }
+
+    @Test
+    public void resultMeshHasNoDuplicatedFaces() {
+        assertTrue(MeshTestUtil.meshHasNoDuplicatedFaces(cube));
+    }
+
+    @Test
+    public void resultMeshIsManifold() {
+        assertTrue(MeshTestUtil.isManifold(cube));
+    }
+
+    @Test
+    public void resultMeshContainsFaceCentersOfOriginalCube() {
+        Mesh3D original = new CubeCreator().create();
+        for (Face3D face : original.getFaces()) {
+            Vector3f faceCenter = original.calculateFaceCenter(face);
+            assertTrue(cube.getVertices().contains(faceCenter));
+        }
+    }
+
+    @Test
+    public void resultMeshContainsOriginalCubeCoordinates() {
+        Mesh3D original = new CubeCreator().create();
+        for (Vector3f v : original.getVertices())
+            assertTrue(cube.getVertices().contains(v));
+    }
+
+    @Test
+    public void testTopFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectTopFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(-1, v.getY(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void testBottomFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectBottomFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(1, v.getY(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void testLeftFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectLeftFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(-1, v.getX(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void testRightFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectRightFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(1, v.getX(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void testBackFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectBackFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(-1, v.getZ(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void testFrontFaceNormalsDirection() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectFrontFaces();
+        for (Face3D face : selection.getFaces()) {
+            for (int i = 0; i < face.indices.length; i++) {
+                Vector3f v = cube.getVertexAt(face.indices[i]);
+                assertEquals(1, v.getZ(), 0);
+            }
+        }
+    }
+
+    @Test
+    public void resultMeshHasFourTopFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectTopFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+    @Test
+    public void resultMeshHasFourBottomFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectBottomFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+    @Test
+    public void resultMeshHasFourLeftFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectLeftFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+    @Test
+    public void resultMeshHasFourRightFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectRightFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+    @Test
+    public void resultMeshHasFourFrontFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectFrontFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+    @Test
+    public void resultMeshHasFourBackFaces() {
+        FaceSelection selection = new FaceSelection(cube);
+        selection.selectBackFaces();
+        assertEquals(4, selection.getFaces().size());
+    }
+
+}
