a0e908f2d51f0984a14c1667ee3a74c43dfefbb9
Simon Dietz <h.s.dietz@web.de>
2024-11-16 02:29:08 +0100

    Using copy.

--- MeshLibCoreClean2022/src/mesh/Face3D.java
+++ MeshLibCoreClean2022/src/mesh/Face3D.java
@@ -46,7 +46,7 @@ public class Face3D {
 
     public Face3D(Face3D f) {
         this(f.indices);
-        this.tag = new String(f.tag);
+        this.tag = String.copyValueOf(f.tag);
     }
 
     @Override
