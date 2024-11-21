ba08c8e7880f4ce38051210575d3c3455fc64bc6
Simon Dietz <h.s.dietz@web.de>
2024-11-21 20:10:13 +0100

    Added test that covers the fixed bug.

---
+++ src/test/java/bugs/VariableCylinderBugTest.java
@@ -0,0 +1,28 @@
+package bugs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import mesh.Mesh3D;
+import mesh.creator.special.VariableCylinderCreator;
+import util.MeshTestUtil;
+
+public class VariableCylinderBugTest {
+
+    /**
+     * Bottom cap was rotated around the wrong axis. Bug fixed: rotate around x
+     * instead of z Wrong rotation makes this test fail.
+     */
+    @Test
+    public void triggerBug() {
+        float rotationSegments = 15.850006f;
+        VariableCylinderCreator creator = new VariableCylinderCreator();
+        creator.setRotationSegments((int) rotationSegments);
+        creator.add(1, 0);
+        creator.add(2, 0);
+        Mesh3D mesh = creator.create();
+        assertTrue(MeshTestUtil.isManifold(mesh));
+    }
+
+}
