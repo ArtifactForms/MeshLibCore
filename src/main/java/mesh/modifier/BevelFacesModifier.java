5ed0c3663365f4a7b603a6ed19aa7d065d57fbe6
Simon Dietz <h.s.dietz@web.de>
2024-11-22 17:48:21 +0100

    Bug fix.

--- src/main/java/mesh/modifier/BevelFacesModifier.java
+++ src/main/java/mesh/modifier/BevelFacesModifier.java
@@ -1,11 +1,7 @@
 package mesh.modifier;
 
-import java.util.ArrayList;
-
 import math.Mathf;
-import mesh.Face3D;
 import mesh.Mesh3D;
-import mesh.util.Mesh3DUtil;
 
 public class BevelFacesModifier implements IMeshModifier {
 
@@ -23,15 +19,7 @@ public class BevelFacesModifier implements IMeshModifier {
     public Mesh3D modify(Mesh3D mesh) {
         float scale = 1f - size;
         float amount = size;
-
-        ArrayList<Face3D> faces = new ArrayList<Face3D>(mesh.faces);
-
-        for (Face3D face : faces) {
-            Mesh3DUtil.extrudeFace(mesh, face, scale, amount);
-        }
-
-        mesh.faces.removeAll(faces);
-
+        mesh.apply(new ExtrudeModifier(scale, amount));
         return mesh;
     }
 
---
+++ src/test/java/bugs/BevelFacesModifierBug.java
@@ -0,0 +1,34 @@
+package bugs;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import mesh.Mesh3D;
+import mesh.creator.platonic.IcosahedronCreator;
+import mesh.modifier.BevelFacesModifier;
+import util.MeshTestUtil;
+
+/**
+ * Modifier removed faces.
+ */
+public class BevelFacesModifierBug {
+
+    @Test
+    public void test() {
+        IcosahedronCreator creator = new IcosahedronCreator();
+        Mesh3D mesh = creator.create();
+        mesh.apply(new BevelFacesModifier());
+        assertTrue(MeshTestUtil.isManifold(mesh));
+    }
+
+    @Test
+    public void testexpectedFaceCount() {
+        int expectedFaceCount = 80;
+        Mesh3D mesh = new IcosahedronCreator().create();
+        mesh.apply(new BevelFacesModifier());
+        assertEquals(expectedFaceCount, mesh.getFaceCount());
+    }
+
+}
