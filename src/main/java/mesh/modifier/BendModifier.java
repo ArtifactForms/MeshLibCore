a6798200a9b983be8c3ad8e0578d16c0f8dba8dc
Simon Dietz <h.s.dietz@web.de>
2024-11-27 19:15:30 +0100

    Refactoring. Extracted epsilon constant. Removed not needed local variables. Renamed variables. Added java doc class description.

--- src/main/java/mesh/modifier/BendModifier.java
+++ src/main/java/mesh/modifier/BendModifier.java
@@ -5,50 +5,48 @@ import math.Vector3f;
 import mesh.Mesh3D;
 
 /**
- * https://www.reddit.com/r/blender/comments/1x10ba/
- * help_with_understanding_the_simple_deform_bend/
+ * A modifier that bends a mesh along the X-axis.
+ * 
+ * This modifier applies a simple bending deformation to each vertex of the
+ * mesh. The degree of bending is controlled by the `factor` parameter. A higher
+ * factor results in a more pronounced bend.
  */
 public class BendModifier implements IMeshModifier {
 
-    private float factor;
+	private static final float EPSILON = 1e-7f;
 
-    public BendModifier(float factor) {
-        this.factor = factor;
-    }
+	private float factor;
 
-    private void simpleDeformBend(float factor, Vector3f v) {
-        float x = v.getX();
-        float y = v.getY();
-        float z = v.getZ();
-        float theta;
-        float sint;
-        float cost;
+	public BendModifier(float factor) {
+		this.factor = factor;
+	}
 
-        theta = x * factor;
-        sint = Mathf.sin(theta);
-        cost = Mathf.cos(theta);
+	private void simpleDeformBend(float factor, Vector3f v) {
+		float theta = v.x * factor;
+		float sinTheta = Mathf.sin(theta);
+		float cosTheta = Mathf.cos(theta);
 
-        if (Mathf.abs(factor) > 1e-7f) {
-            float bx = -(y - 1.0f / factor) * sint;
-            float by = (y - 1.0f / factor) * cost + 1.0f / factor;
-            float bz = z;
-            v.set(bx, by, bz);
-        }
-    }
+		if (Mathf.abs(factor) > EPSILON) {
+			float bx = -(v.y - 1.0f / factor) * sinTheta;
+			float by = (v.y - 1.0f / factor) * cosTheta + 1.0f / factor;
+			float bz = v.z;
+			v.set(bx, by, bz);
+		}
+	}
 
-    @Override
-    public Mesh3D modify(Mesh3D mesh) {
-        for (Vector3f v : mesh.vertices)
-            simpleDeformBend(factor, v);
-        return mesh;
-    }
+	@Override
+	public Mesh3D modify(Mesh3D mesh) {
+		for (Vector3f v : mesh.vertices)
+			simpleDeformBend(factor, v);
+		return mesh;
+	}
 
-    public float getFactor() {
-        return factor;
-    }
+	public float getFactor() {
+		return factor;
+	}
 
-    public void setFactor(float factor) {
-        this.factor = factor;
-    }
+	public void setFactor(float factor) {
+		this.factor = factor;
+	}
 
 }
