f973fdb067d117a6a49617e71a1a8a060395ad01
Simon Dietz <h.s.dietz@web.de>
2024-12-04 16:11:51 +0100

    Using jupiter api.

--- src/test/java/mesh/creator/primitives/CubeCreatorTest.java
+++ src/test/java/mesh/creator/primitives/CubeCreatorTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
