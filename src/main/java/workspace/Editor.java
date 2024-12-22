3d7ea252c5d609466a7403a30d655de8cf8f3435
Simon Dietz <h.s.dietz@web.de>
2024-12-22 14:13:42 +0100

    Format changes.

--- src/main/java/workspace/Editor.java
+++ src/main/java/workspace/Editor.java
@@ -23,153 +23,152 @@ import workspace.ui.elements.ViewportCompass;
 
 public class Editor implements ModelListener {
 
-	protected List<SceneObject> sceneObjects;
+  protected List<SceneObject> sceneObjects;
 
-	protected UiComponent rootUi;
+  protected UiComponent rootUi;
 
-	protected KeyCommandMap commands;
+  protected KeyCommandMap commands;
 
-	protected WorkspaceModel model;
+  protected WorkspaceModel model;
 
-	protected ViewportCompass gizmo;
+  protected ViewportCompass gizmo;
 
-	protected WorkspaceSideBarUi sideBar;
+  protected WorkspaceSideBarUi sideBar;
 
-	protected UiEditorMenu menu;
+  protected UiEditorMenu menu;
 
-	public Editor() {
-		setup();
-	}
+  public Editor() {
+    setup();
+  }
 
-	private void setup() {
-		setupLookAndFeel();
-		initializeModel();
-		initializeSceneObjects();
-		initializeRootUi();
-		createUi();
-		initializeCommandMap();
-		registerKeyCommands();
-	}
+  private void setup() {
+    setupLookAndFeel();
+    initializeModel();
+    initializeSceneObjects();
+    initializeRootUi();
+    createUi();
+    initializeCommandMap();
+    registerKeyCommands();
+  }
 
-	@Override
-	public void onModelChanged() {
-		rootUi.setVisible(model.isUiVisible());
-	}
+  @Override
+  public void onModelChanged() {
+    rootUi.setVisible(model.isUiVisible());
+  }
 
-	private void initializeSceneObjects() {
-		sceneObjects = new ArrayList<SceneObject>();
-	}
+  private void initializeSceneObjects() {
+    sceneObjects = new ArrayList<SceneObject>();
+  }
 
-	private void createUi() {
-		rootUi.add(getSideBar());
-		rootUi.add(getGizmo());
-		rootUi.add(getMenu());
-	}
+  private void createUi() {
+    rootUi.add(getSideBar());
+    rootUi.add(getGizmo());
+    rootUi.add(getMenu());
+  }
 
-	private WorkspaceSideBarUi getSideBar() {
-		if (sideBar == null) {
-			sideBar = new WorkspaceSideBarUi(model);
-		}
-		return sideBar;
-	}
+  private WorkspaceSideBarUi getSideBar() {
+    if (sideBar == null) {
+      sideBar = new WorkspaceSideBarUi(model);
+    }
+    return sideBar;
+  }
 
-	private ViewportCompass getGizmo() {
-		if (gizmo == null) {
-			gizmo = new ViewportCompass();
-		}
-		return gizmo;
-	}
+  private ViewportCompass getGizmo() {
+    if (gizmo == null) {
+      gizmo = new ViewportCompass();
+    }
+    return gizmo;
+  }
 
-	private UiEditorMenu getMenu() {
-		if (menu == null) {
-			menu = new UiEditorMenu();
-		}
-		return menu;
-	}
+  private UiEditorMenu getMenu() {
+    if (menu == null) {
+      menu = new UiEditorMenu();
+    }
+    return menu;
+  }
 
-	private void initializeRootUi() {
-		rootUi = new UiComponent();
-	}
+  private void initializeRootUi() {
+    rootUi = new UiComponent();
+  }
 
-	private void setupLookAndFeel() {
-		LookAndFeel.setup();
-	}
+  private void setupLookAndFeel() {
+    LookAndFeel.setup();
+  }
 
-	private void initializeCommandMap() {
-		commands = new KeyCommandMap();
-	}
+  private void initializeCommandMap() {
+    commands = new KeyCommandMap();
+  }
 
-	private void initializeModel() {
-		model = new WorkspaceModel();
-	}
+  private void initializeModel() {
+    model = new WorkspaceModel();
+  }
 
-	private void registerKeyCommands() {
-		commands.register(new ShowHideGridCommand(model));
-		commands.register(new ShowHideXAxisCommand(model));
-		commands.register(new ShowHideYAxisCommand(model));
-		commands.register(new ShowHideZAxisCommand(model));
-		commands.register(new ShowHideSideBarCommand(model));
-		commands.register(new ShowHideFaceNormalsCommand(model));
-		commands.register(new ResetPanningCommand(model));
-		commands.register(new ShowHideVertexNormalsCommand(model));
-		commands.register(new ShowHideEdgesCommand(model));
-		commands.register(new WireframeCommand(model));
-		commands.register(new ShadeSmoothFlatCommand(model));
-	}
+  private void registerKeyCommands() {
+    commands.register(new ShowHideGridCommand(model));
+    commands.register(new ShowHideXAxisCommand(model));
+    commands.register(new ShowHideYAxisCommand(model));
+    commands.register(new ShowHideZAxisCommand(model));
+    commands.register(new ShowHideSideBarCommand(model));
+    commands.register(new ShowHideFaceNormalsCommand(model));
+    commands.register(new ResetPanningCommand(model));
+    commands.register(new ShowHideVertexNormalsCommand(model));
+    commands.register(new ShowHideEdgesCommand(model));
+    commands.register(new WireframeCommand(model));
+    commands.register(new ShadeSmoothFlatCommand(model));
+  }
 
-	private void resizeRootUi(int x, int y, int width, int height) {
-		rootUi.setX(x);
-		rootUi.setY(y);
-		rootUi.setWidth(width);
-		rootUi.setHeight(height);
-	}
+  private void resizeRootUi(int x, int y, int width, int height) {
+    rootUi.setX(x);
+    rootUi.setY(y);
+    rootUi.setWidth(width);
+    rootUi.setHeight(height);
+  }
 
-	public void resize(int x, int y, int width, int height) {
-		resizeRootUi(x, y, width, height);
-		updateGizmo(width, height);
-	}
+  public void resize(int x, int y, int width, int height) {
+    resizeRootUi(x, y, width, height);
+    updateGizmo(width, height);
+  }
 
-	public void handleMouseClicked(int x, int y) {
-		rootUi.onMouseClicked(x, y);
-	}
+  public void handleMouseClicked(int x, int y) {
+    rootUi.onMouseClicked(x, y);
+  }
 
-	public void handleMousePressed(int x, int y) {
-		rootUi.onMousePressed(x, y);
-	}
+  public void handleMousePressed(int x, int y) {
+    rootUi.onMousePressed(x, y);
+  }
 
-	public void handleMouseDragged(int x, int y) {
-		rootUi.onMouseDragged(x, y);
-	}
+  public void handleMouseDragged(int x, int y) {
+    rootUi.onMouseDragged(x, y);
+  }
 
-	public void handleMouseReleased(int x, int y) {
-		rootUi.onMouseReleased(x, y);
-	}
+  public void handleMouseReleased(int x, int y) {
+    rootUi.onMouseReleased(x, y);
+  }
 
-	public void handleMouseWheel(float amount) {
-		float scale = model.getScale();
-		scale -= amount * scale * 0.2f;
-		model.setScale(scale);
-	}
+  public void handleMouseWheel(float amount) {
+    float scale = model.getScale();
+    scale -= amount * scale * 0.2f;
+    model.setScale(scale);
+  }
 
-	private void updateGizmo(int width, int height) {
-		gizmo.setX(width - 80);
-		gizmo.setY(130);
-	}
+  private void updateGizmo(int width, int height) {
+    gizmo.setX(width - 80);
+    gizmo.setY(130);
+  }
 
-	public void add(UiComponent component) {
-		rootUi.add(component);
-	}
+  public void add(UiComponent component) {
+    rootUi.add(component);
+  }
 
-	public void addSceneObject(SceneObject sceneObject) {
-		sceneObjects.add(sceneObject);
-	}
+  public void addSceneObject(SceneObject sceneObject) {
+    sceneObjects.add(sceneObject);
+  }
 
-	public void addAll(Collection<SceneObject> sceneObjects) {
-		this.sceneObjects.addAll(sceneObjects);
-	}
+  public void addAll(Collection<SceneObject> sceneObjects) {
+    this.sceneObjects.addAll(sceneObjects);
+  }
 
-	public void clearSceneObjects() {
-		sceneObjects.clear();
-	}
-
+  public void clearSceneObjects() {
+    sceneObjects.clear();
+  }
 }
