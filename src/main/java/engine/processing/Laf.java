f5cc2f3e2e275d5e324c6165abb0a7da8d5ad949
Simon Dietz <h.s.dietz@web.de>
2024-12-22 14:20:17 +0100

    Fix. Added missing Laf.

---
+++ src/main/java/engine/processing/Laf.java
@@ -0,0 +1,14 @@
+package engine.processing;
+
+import math.Color;
+
+public class Laf {
+
+  public static final Color LIGHT_GIZMO_X = Color.getColorFromInt(221, 56, 79);
+
+  public static final Color LIGHT_GIZMO_Y = Color.getColorFromInt(120, 181, 22);
+
+  public static final Color LIGHT_GIZMO_Z = Color.getColorFromInt(44, 142, 252);
+
+  public static final Color LIGHT_GIZMO_DIRECTIONAL = Color.YELLOW;
+}
