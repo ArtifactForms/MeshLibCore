01e2d5106f5b1e49805d0b383bee72d7aec2a262
Simon Dietz <h.s.dietz@web.de>
2025-01-15 20:12:05 +0100

    Feat: Added hierarchy print.

--- src/main/java/engine/animation/skeleton/Skeleton.java
+++ src/main/java/engine/animation/skeleton/Skeleton.java
@@ -1,25 +1,31 @@
 package engine.animation.skeleton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Skeleton {
 
   private Bone rootBone;
 
-  private final List<Bone> allBones;
+  private final Map<String, Bone> allBones;
 
   public Skeleton(Bone rootBone) {
     this.rootBone = rootBone;
-    this.allBones = new ArrayList<>();
+    this.allBones = new HashMap<>();
     collectBones(rootBone);
   }
 
   private void collectBones(Bone bone) {
-    allBones.add(bone);
+    allBones.put(bone.getName(), bone);
     for (Bone child : bone.getChildren()) {
       collectBones(child);
     }
+  }
+
+  public Bone getBone(String name) {
+    return allBones.get(name);
   }
 
   public void update() {
@@ -39,6 +45,20 @@ public class Skeleton {
   }
 
   public List<Bone> getAllBones() {
-    return allBones;
+    return new ArrayList<>(allBones.values());
+  }
+
+  public void printHierarchy() {
+    printHierarchy(rootBone, 0);
+  }
+
+  private void printHierarchy(Bone bone, int level) {
+    for (int i = 0; i < level; i++) {
+      System.out.print("  ");
+    }
+    System.out.println(bone.getName());
+    for (Bone child : bone.getChildren()) {
+      printHierarchy(child, level + 1);
+    }
   }
 }
