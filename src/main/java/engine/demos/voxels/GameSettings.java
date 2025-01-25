2a2f1a605219f2f968329cdf046e2f44ec4cbfe1
Simon Dietz <h.s.dietz@web.de>
2025-01-25 13:36:28 +0100

    Added game settings.

---
+++ src/main/java/engine/demos/voxels/GameSettings.java
@@ -0,0 +1,14 @@
+package engine.demos.voxels;
+
+public class GameSettings {
+
+  public static boolean textureNoise = true;
+
+  public static boolean textureDebugText = true;
+  
+  public static boolean textureBackground = false;
+  
+  public static int renderDistance = 16;
+  
+  public static float flySpeed = 32;
+}
