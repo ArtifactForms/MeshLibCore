4c974c8cb0de9c45154bd50bf643cd3051455289
Simon Dietz <h.s.dietz@web.de>
2026-02-07 15:21:09 +0100

    added doc

--- src/main/java/mesh/Axis.java
+++ src/main/java/mesh/Axis.java
@@ -1,7 +1,37 @@
 package mesh;
 
+/**
+ * Defines a 2D plane in 3D space by specifying the two axes it spans.
+ *
+ * <p>This enum is typically used for operations that work on planar projections of 3D data, such
+ * as:
+ *
+ * <ul>
+ *   <li>Mesh slicing
+ *   <li>UV generation
+ *   <li>2D projections of 3D geometry
+ * </ul>
+ */
 public enum Axis {
+
+  /**
+   * Plane spanned by the X and Y axes.
+   *
+   * <p>The Z axis is treated as the perpendicular (normal) axis.
+   */
   XY,
+
+  /**
+   * Plane spanned by the X and Z axes.
+   *
+   * <p>The Y axis is treated as the perpendicular (normal) axis.
+   */
   XZ,
+
+  /**
+   * Plane spanned by the Y and Z axes.
+   *
+   * <p>The X axis is treated as the perpendicular (normal) axis.
+   */
   YZ
 }
