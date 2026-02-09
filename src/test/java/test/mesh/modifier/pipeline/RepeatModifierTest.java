1dc097e3b8a3537b8765977f0ecc7798491f3800
Simon Dietz <h.s.dietz@web.de>
2026-02-09 11:14:56 +0100

    moved package

--- src/test/java/test/mesh/modifier/pipeline/RepeatModifierTest.java
+++ src/test/java/test/mesh/modifier/pipeline/RepeatModifierTest.java
@@ -1,4 +1,4 @@
-package mesh.modifier.pipeline;
+package test.mesh.modifier.pipeline;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
