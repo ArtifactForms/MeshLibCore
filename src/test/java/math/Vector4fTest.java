aaee991345e41776591730672924033f66b20bae
Simon Dietz <h.s.dietz@web.de>
2026-02-12 22:44:08 +0100

    Migrated test to JUnit Jupiter

--- src/test/java/math/Vector4fTest.java
+++ src/test/java/math/Vector4fTest.java
@@ -1,11 +1,11 @@
 package math;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
