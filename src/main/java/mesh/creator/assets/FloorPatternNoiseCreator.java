612a40d8e82a60fa34b10cd50d59db340666a9c1
Simon Dietz <h.s.dietz@web.de>
2026-02-14 12:06:37 +0100

    Make FloorPatternNoiseCreator deterministic by default
    FloorPatternNoiseCreator now uses a seeded Random instance to ensure
    reproducible mesh generation.
    
    This is an intentional behavior change to guarantee deterministic
    output.
    
    Updated reference OBJ and characterization test to reflect
    the new deterministic behavior.

--- src/main/java/mesh/creator/assets/FloorPatternNoiseCreator.java
+++ src/main/java/mesh/creator/assets/FloorPatternNoiseCreator.java
@@ -1,5 +1,7 @@
 package mesh.creator.assets;
 
+import java.util.Random;
+
 import math.Mathf;
 import mesh.Face3D;
 import mesh.Mesh3D;
@@ -7,105 +9,123 @@ import mesh.creator.IMeshCreator;
 import mesh.creator.primitives.GridCreator;
 import mesh.modifier.topology.ExtrudeModifier;
 import mesh.modifier.topology.SolidifyModifier;
+import mesh.modifier.transform.TranslateModifier;
 import mesh.selection.FaceSelection;
 import mesh.util.Mesh3DUtil;
 
-/**
- * Inspiration: https://www.youtube.com/watch?v=aFkBHigEOdE
- */
+/** Inspiration: https://www.youtube.com/watch?v=aFkBHigEOdE */
 public class FloorPatternNoiseCreator implements IMeshCreator {
 
-    private float height;
+  private float height;
 
-    private float radius;
+  private float radius;
 
-    private int subdivisions;
+  private int subdivisions;
 
-    private Mesh3D mesh;
+  private long seed;
 
-    private FaceSelection faceSelection;
+  private Mesh3D mesh;
 
-    public FloorPatternNoiseCreator() {
-        this(0.2f, 2, 4);
+  private FaceSelection faceSelection;
+
+  private Random random;
+
+  public FloorPatternNoiseCreator() {
+    this(0.2f, 2, 4);
+  }
+
+  public FloorPatternNoiseCreator(float height, float radius, int subdivisions) {
+    this.height = height;
+    this.radius = radius;
+    this.subdivisions = subdivisions;
+  }
+
+  @Override
+  public Mesh3D create() {
+    initializeRandomWithSeed();
+    createGrid();
+    initializeFaceSelection();
+    selectAllFaces();
+    solidify();
+    extrude();
+    snapToGround();
+    rotateFaces();
+    return mesh;
+  }
+
+  private void initializeRandomWithSeed() {
+    this.random = new Random(seed);
+  }
+
+  private void rotateFaces() {
+    for (Face3D face : faceSelection.getFaces()) {
+      Mesh3DUtil.rotateFaceZ(mesh, face, randomAngle());
+      Mesh3DUtil.rotateFaceX(mesh, face, randomAngle());
     }
+  }
 
-    public FloorPatternNoiseCreator(float height, float radius,
-            int subdivisions) {
-        this.height = height;
-        this.radius = radius;
-        this.subdivisions = subdivisions;
-    }
+  private float randomAngle() {
+    return Mathf.toRadians(createRandomValue(0, 2));
+  }
 
-    @Override
-    public Mesh3D create() {
-        createGrid();
-        initializeFaceSelection();
-        selectAllFaces();
-        solidify();
-        extrude();
-        snapToGround();
-        rotateFaces();
-        return mesh;
-    }
+  private float createRandomValue(float minimum, float maximum) {
+    return minimum + random.nextFloat() * (maximum - minimum);
+  }
 
-    private void rotateFaces() {
-        for (Face3D face : faceSelection.getFaces()) {
-            Mesh3DUtil.rotateFaceZ(mesh, face, randomAngle());
-            Mesh3DUtil.rotateFaceX(mesh, face, randomAngle());
-        }
-    }
+  private void snapToGround() {
+    new TranslateModifier(0, -height * 0.5f, 0).modify(mesh);
+  }
 
-    private float randomAngle() {
-        return Mathf.toRadians(Mathf.random(0, 2));
-    }
+  private void extrude() {
+    new ExtrudeModifier(0.9f, height * 0.5f).modify(mesh, faceSelection.getFaces());
+  }
 
-    private void snapToGround() {
-        mesh.translateY(-height * 0.5f);
-    }
+  private void solidify() {
+    new SolidifyModifier(height * 0.5f).modify(mesh);
+  }
 
-    private void extrude() {
-        new ExtrudeModifier(0.9f, height * 0.5f).modify(mesh,
-                faceSelection.getFaces());
-    }
+  private void initializeFaceSelection() {
+    faceSelection = new FaceSelection(mesh);
+  }
 
-    private void solidify() {
-        new SolidifyModifier(height * 0.5f).modify(mesh);
-    }
+  private void selectAllFaces() {
+    faceSelection.selectAll();
+  }
 
-    private void initializeFaceSelection() {
-        faceSelection = new FaceSelection(mesh);
-    }
+  private void createGrid() {
+    mesh = new GridCreator(subdivisions, subdivisions, radius).create();
+  }
 
-    private void selectAllFaces() {
-        faceSelection.selectAll();
-    }
+  public long getSeed() {
+    return seed;
+  }
 
-    private void createGrid() {
-        mesh = new GridCreator(subdivisions, subdivisions, radius).create();
-    }
+  public void setSeed(long seed) {
+    this.seed = seed;
+    random = new Random(seed);
+  }
 
-    public float getHeight() {
-        return height;
-    }
+  public float getHeight() {
+    return height;
+  }
 
-    public void setHeight(float height) {
-        this.height = height;
-    }
+  public void setHeight(float height) {
+    this.height = height;
+  }
 
-    public float getRadius() {
-        return radius;
-    }
+  public float getRadius() {
+    return radius;
+  }
 
-    public void setRadius(float radius) {
-        this.radius = radius;
-    }
+  public void setRadius(float radius) {
+    this.radius = radius;
+  }
 
-    public int getSubdivisions() {
-        return subdivisions;
-    }
+  public int getSubdivisions() {
+    return subdivisions;
+  }
 
-    public void setSubdivisions(int subdivisions) {
-        this.subdivisions = subdivisions;
-    }
-
+  public void setSubdivisions(int subdivisions) {
+    this.subdivisions = subdivisions;
+  }
 }
