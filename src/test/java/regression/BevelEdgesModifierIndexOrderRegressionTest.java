5c3459cff06b7b22c2f032ab637891d032af45e6
Simon Dietz <h.s.dietz@web.de>
2026-02-14 11:34:43 +0100

    Rename test package 'bugs' to 'regression'
    Tests in this package guard against previously fixed defects
    and are therefore regression tests rather than bug tests.

--- src/test/java/regression/BevelEdgesModifierIndexOrderRegressionTest.java
+++ src/test/java/regression/BevelEdgesModifierIndexOrderRegressionTest.java
@@ -1,4 +1,4 @@
-package bugs;
+package regression;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/test/java/regression/BevelFacesModifierBugTest.java
+++ src/test/java/regression/BevelFacesModifierBugTest.java
@@ -1,4 +1,4 @@
-package bugs;
+package regression;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
