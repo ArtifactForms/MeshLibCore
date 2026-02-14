9dd48a138960ce4e8e00b882fc58443d9fc7fa26
Simon Dietz <h.s.dietz@web.de>
2026-02-14 17:54:22 +0100

    Refactor solved deprecated method usage

--- src/main/java/mesh/creator/assets/WoodenBarrelCreator.java
+++ src/main/java/mesh/creator/assets/WoodenBarrelCreator.java
@@ -8,125 +8,137 @@ import mesh.creator.FillType;
 import mesh.creator.IMeshCreator;
 import mesh.creator.primitives.SegmentedCylinderCreator;
 import mesh.modifier.subdivision.PlanarVertexCenterModifier;
+import mesh.modifier.topology.ExtrudeModifier;
+import mesh.modifier.transform.TranslateModifier;
 import mesh.selection.FaceSelection;
-import mesh.util.Mesh3DUtil;
 
 public class WoodenBarrelCreator implements IMeshCreator {
 
-    private float radius;
+  private float radius;
 
-    private float height;
+  private float height;
 
-    private float inset;
+  private float inset;
 
-    private float bendFactor;
+  private float bendFactor;
 
-    private int rotationSegments;
+  private int rotationSegments;
 
-    private int heightSegments;
+  private int heightSegments;
 
-    private Mesh3D mesh;
+  private Mesh3D mesh;
 
-    public WoodenBarrelCreator() {
-        radius = 1.0f;
-        height = 2.0f;
-        inset = 0.05f;
-        bendFactor = 0.75f;
-        rotationSegments = 16;
-        heightSegments = 8;
+  public WoodenBarrelCreator() {
+    radius = 1.0f;
+    height = 2.0f;
+    inset = 0.05f;
+    bendFactor = 0.75f;
+    rotationSegments = 16;
+    heightSegments = 8;
+  }
+
+  @Override
+  public Mesh3D create() {
+    createBaseCylinder();
+    bend();
+    createInsets();
+    center();
+    return mesh;
+  }
+
+  private void createBaseCylinder() {
+    SegmentedCylinderCreator creator = new SegmentedCylinderCreator();
+    creator.setHeight(height);
+    creator.setRotationSegments(rotationSegments);
+    creator.setHeightSegments(heightSegments);
+    creator.setTopRadius(radius);
+    creator.setBottomRadius(radius);
+    creator.setCapFillType(FillType.N_GON);
+    mesh = creator.create();
+  }
+
+  private void bend() {
+    for (Vector3f v : mesh.getVertices(0, mesh.getVertexCount())) {
+      float y = v.getY();
+      float m = Mathf.cos(Mathf.abs(y * bendFactor));
+      v.multLocal(new Vector3f(m, 1, m));
     }
+  }
 
-    @Override
-    public Mesh3D create() {
-        createBaseCylinder();
-        bend();
-        createInsets();
-        center();
-        return mesh;
+  private void createInsets() {
+    FaceSelection selection = new FaceSelection(mesh);
+    selection.selectTopFaces();
+    selection.selectBottomFaces();
+
+    ExtrudeModifier modifier = new ExtrudeModifier();
+    PlanarVertexCenterModifier modifier2 = new PlanarVertexCenterModifier();
+
+    for (Face3D face : selection.getFaces()) {
+      modifier.setScale(0.9f);
+      modifier.setAmount(0);
+      modifier.modify(mesh, face);
+
+      modifier.setScale(1.0f);
+      modifier.setAmount(-inset);
+      modifier.modify(mesh, face);
+
+      modifier.setAmount(0);
+      modifier.setScale(0.9f);
+      modifier.modify(mesh, face);
+
+      modifier2.modify(mesh, face);
     }
+  }
 
-    private void createBaseCylinder() {
-        SegmentedCylinderCreator creator = new SegmentedCylinderCreator();
-        creator.setHeight(height);
-        creator.setRotationSegments(rotationSegments);
-        creator.setHeightSegments(heightSegments);
-        creator.setTopRadius(radius);
-        creator.setBottomRadius(radius);
-        creator.setCapFillType(FillType.N_GON);
-        mesh = creator.create();
-    }
+  private void center() {
+    new TranslateModifier(0, -height * 0.5f, 0).modify(mesh);
+  }
 
-    private void bend() {
-        for (Vector3f v : mesh.getVertices(0, mesh.getVertexCount())) {
-            float y = v.getY();
-            float m = Mathf.cos(Mathf.abs(y * bendFactor));
-            v.multLocal(new Vector3f(m, 1, m));
-        }
-    }
+  public float getRadius() {
+    return radius;
+  }
 
-    private void createInsets() {
-        FaceSelection selection = new FaceSelection(mesh);
-        selection.selectTopFaces();
-        selection.selectBottomFaces();
+  public void setRadius(float radius) {
+    this.radius = radius;
+  }
 
-        for (Face3D face : selection.getFaces()) {
-            Mesh3DUtil.extrudeFace(mesh, face, 0.9f, 0.0f);
-            Mesh3DUtil.extrudeFace(mesh, face, 1.0f, -inset);
-            Mesh3DUtil.extrudeFace(mesh, face, 0.9f, 0.0f);
-            new PlanarVertexCenterModifier().modify(mesh, face);
-        }
-    }
+  public float getHeight() {
+    return height;
+  }
 
-    private void center() {
-        mesh.translateY(-height * 0.5f);
-    }
+  public void setHeight(float height) {
+    this.height = height;
+  }
 
-    public float getRadius() {
-        return radius;
-    }
+  public float getInset() {
+    return inset;
+  }
 
-    public void setRadius(float radius) {
-        this.radius = radius;
-    }
+  public void setInset(float inset) {
+    this.inset = inset;
+  }
 
-    public float getHeight() {
-        return height;
-    }
+  public float getBendFactor() {
+    return bendFactor;
+  }
 
-    public void setHeight(float height) {
-        this.height = height;
-    }
+  public void setBendFactor(float bendFactor) {
+    this.bendFactor = bendFactor;
+  }
 
-    public float getInset() {
-        return inset;
-    }
+  public int getRotationSegments() {
+    return rotationSegments;
+  }
 
-    public void setInset(float inset) {
-        this.inset = inset;
-    }
+  public void setRotationSegments(int rotationSegments) {
+    this.rotationSegments = rotationSegments;
+  }
 
-    public float getBendFactor() {
-        return bendFactor;
-    }
+  public int getHeightSegments() {
+    return heightSegments;
+  }
 
-    public void setBendFactor(float bendFactor) {
-        this.bendFactor = bendFactor;
-    }
-
-    public int getRotationSegments() {
-        return rotationSegments;
-    }
-
-    public void setRotationSegments(int rotationSegments) {
-        this.rotationSegments = rotationSegments;
-    }
-
-    public int getHeightSegments() {
-        return heightSegments;
-    }
-
-    public void setHeightSegments(int heightSegments) {
-        this.heightSegments = heightSegments;
-    }
-
+  public void setHeightSegments(int heightSegments) {
+    this.heightSegments = heightSegments;
+  }
 }
