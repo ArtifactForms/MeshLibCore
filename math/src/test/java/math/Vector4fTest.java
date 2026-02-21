5c62177f08b69c8bfe289d919ab57706a3bd1b2a
Simon Dietz <h.s.dietz@web.de>
2026-02-21 12:16:20 +0100

    moved math specific tests from core to math module

--- math/src/test/java/math/Vector4fTest.java
+++ math/src/test/java/math/Vector4fTest.java
@@ -1,5 +1,6 @@
 package math;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
