93d91b17a1deb0d84da539b604660cd94dd49783
Simon Dietz <h.s.dietz@web.de>
2026-03-17 00:27:43 +0100

    update

--- voxels-common/src/main/java/common/game/ItemStack.java
+++ voxels-common/src/main/java/common/game/ItemStack.java
@@ -18,9 +18,13 @@ public class ItemStack {
    * @param amount The initial quantity of items in the stack.
    */
   public ItemStack(short itemId, int amount) {
+    if (amount < 0) {
+      throw new IllegalArgumentException("Amount cannot be negative");
+    }
     this.itemId = itemId;
     this.amount = amount;
   }
+
   /**
    * Attempts to add a quantity to the current stack without exceeding the maximum limit. * @param
    * amountToAdd The quantity to be added to this stack.
@@ -29,17 +33,28 @@ public class ItemStack {
    * @return The remainder that could not fit into the stack (0 if everything fit).
    */
   public int add(int amountToAdd, int maxStackSize) {
+    if (amountToAdd <= 0) {
+      return 0;
+    }
+
+    if (maxStackSize <= 0) {
+      throw new IllegalArgumentException("maxStackSize must be positive");
+    }
+
     int space = maxStackSize - this.amount;
     int canAdd = Math.min(space, amountToAdd);
 
     this.amount += canAdd;
 
-    // Return the "overflow" value
     return amountToAdd - canAdd;
   }
-  
+
   public ItemStack copy() {
     return new ItemStack(itemId, amount);
+  }
+
+  public void clear() {
+    this.amount = 0;
   }
 
   /** Gets the unique ID of the item in this stack. * @return The item ID. */
@@ -53,10 +68,31 @@ public class ItemStack {
   }
 
   public void setAmount(int amount) {
+    if (amount < 0) {
+      throw new IllegalArgumentException("Amount cannot be negative");
+    }
     this.amount = amount;
   }
 
   public boolean isEmpty() {
     return amount == 0;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ItemStack)) return false;
+    ItemStack other = (ItemStack) o;
+    return itemId == other.itemId && amount == other.amount;
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * itemId + amount;
+  }
+
+  @Override
+  public String toString() {
+    return "ItemStack{id=" + itemId + ", amount=" + amount + "}";
   }
 }
