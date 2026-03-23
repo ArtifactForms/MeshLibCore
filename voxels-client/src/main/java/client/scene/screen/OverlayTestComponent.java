f355f430b1b2229795b6b6c0244b1df07dc6b012
Simon Dietz <h.s.dietz@web.de>
2026-03-23 09:44:35 +0100

    overlay

---
+++ voxels-client/src/main/java/client/scene/screen/OverlayTestComponent.java
@@ -0,0 +1,28 @@
+package client.scene.screen;
+
+import client.app.GameClient;
+import engine.components.AbstractComponent;
+import engine.runtime.input.Input;
+import engine.runtime.input.Key;
+
+public class OverlayTestComponent extends AbstractComponent {
+
+  private Input input;
+  private GameClient client;
+
+  public OverlayTestComponent(Input input, GameClient client) {
+    this.input = input;
+    this.client = client;
+  }
+
+  @Override
+  public void onUpdate(float tpf) {
+    if (getOwner().getScene().getTopScreen() instanceof OverlayTestScreen) {
+      return;
+    }
+
+    if (input.wasKeyPressed(Key.O)) {
+      getOwner().getScene().pushScreen(new OverlayTestScreen(client));
+    }
+  }
+}
