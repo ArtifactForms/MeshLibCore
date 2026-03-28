90c6d1a1db9940d64274e61ce5a916fb96cf22b0
Simon Dietz <h.s.dietz@web.de>
2026-03-28 11:05:07 +0100

    add ping component

---
+++ voxels-client/src/main/java/client/ui/PingComponent.java
@@ -0,0 +1,30 @@
+package client.ui;
+
+import client.app.GameClient;
+import engine.components.AbstractComponent;
+import engine.components.RenderableComponent;
+import engine.rendering.Graphics;
+import engine.resources.Font;
+import math.Color;
+
+public class PingComponent extends AbstractComponent implements RenderableComponent {
+
+  private GameClient client;
+  private Font font;
+
+  public PingComponent(GameClient client) {
+    this.client = client;
+    this.font = new Font("monogram-extended", 32, Font.PLAIN);
+  }
+
+  @Override
+  public void render(Graphics g) {
+    g.setColor(Color.WHITE);
+    g.setFont(font);
+
+    String text = "" + client.getPingTracker().getPing() + " ms";
+    float x = g.getWidth() - g.textWidth(text) - 20;
+    float y = g.getHeight() - 20;
+    g.text(text, x, y);
+  }
+}
