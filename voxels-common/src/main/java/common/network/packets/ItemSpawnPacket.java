842f299decdfd4613b15de3200ee871e5334adae
Simon Dietz <h.s.dietz@web.de>
2026-04-04 14:07:39 +0200

    format

--- voxels-common/src/main/java/common/network/packets/ItemSpawnPacket.java
+++ voxels-common/src/main/java/common/network/packets/ItemSpawnPacket.java
@@ -9,68 +9,98 @@ import common.network.Packet;
 import common.network.PacketBuffer;
 import common.network.PacketIds;
 
-/**
- * Sent by the server to tell clients to spawn a new item entity in the world.
- */
+/** Sent by the server to tell clients to spawn a new item entity in the world. */
 public class ItemSpawnPacket implements Packet {
 
-    private long entityId;
-    private short blockTypeId;
-    private float x, y, z;
-    private float velX, velY, velZ;
+  private long entityId;
 
-    public ItemSpawnPacket() {}
+  private short blockTypeId;
 
-    /**
-     * Creates a spawn packet based on an existing ItemEntity.
-     */
-    public ItemSpawnPacket(ItemEntity entity) {
-        this.entityId = entity.getEntityId();
-        this.blockTypeId = entity.getBlockType().getId();
-        this.x = entity.getPosition().x;
-        this.y = entity.getPosition().y;
-        this.z = entity.getPosition().z;
-        this.velX = entity.getVelocity().x;
-        this.velY = entity.getVelocity().y;
-        this.velZ = entity.getVelocity().z;
-    }
+  private float x;
 
-    @Override
-    public int getId() {
-        return PacketIds.ITEM_SPAWN;
-    }
+  private float y;
 
-    @Override
-    public void write(PacketBuffer out) throws IOException {
-        out.writeLong(entityId);
-        out.writeShort(blockTypeId);
-        out.writeFloat(x);
-        out.writeFloat(y);
-        out.writeFloat(z);
-        out.writeFloat(velX);
-        out.writeFloat(velY);
-        out.writeFloat(velZ);
-    }
+  private float z;
 
-    @Override
-    public void read(PacketBuffer in) throws IOException {
-        this.entityId = in.readLong();
-        this.blockTypeId = in.readShort();
-        this.x = in.readFloat();
-        this.y = in.readFloat();
-        this.z = in.readFloat();
-        this.velX = in.readFloat();
-        this.velY = in.readFloat();
-        this.velZ = in.readFloat();
-    }
+  private float velX;
 
-    // Getters for the Client Dispatcher
-    public long getEntityId() { return entityId; }
-    public BlockType getBlockType() { return BlockRegistry.get(blockTypeId); }
-    public float getX() { return x; }
-    public float getY() { return y; }
-    public float getZ() { return z; }
-    public float getVelX() { return velX; }
-    public float getVelY() { return velY; }
-    public float getVelZ() { return velZ; }
+  private float velY;
+
+  private float velZ;
+
+  public ItemSpawnPacket() {}
+
+  /** Creates a spawn packet based on an existing ItemEntity. */
+  public ItemSpawnPacket(ItemEntity entity) {
+    this.entityId = entity.getEntityId();
+    this.blockTypeId = entity.getBlockType().getId();
+    this.x = entity.getPosition().x;
+    this.y = entity.getPosition().y;
+    this.z = entity.getPosition().z;
+    this.velX = entity.getVelocity().x;
+    this.velY = entity.getVelocity().y;
+    this.velZ = entity.getVelocity().z;
+  }
+
+  @Override
+  public int getId() {
+    return PacketIds.ITEM_SPAWN;
+  }
+
+  @Override
+  public void write(PacketBuffer out) throws IOException {
+    out.writeLong(entityId);
+    out.writeShort(blockTypeId);
+    out.writeFloat(x);
+    out.writeFloat(y);
+    out.writeFloat(z);
+    out.writeFloat(velX);
+    out.writeFloat(velY);
+    out.writeFloat(velZ);
+  }
+
+  @Override
+  public void read(PacketBuffer in) throws IOException {
+    this.entityId = in.readLong();
+    this.blockTypeId = in.readShort();
+    this.x = in.readFloat();
+    this.y = in.readFloat();
+    this.z = in.readFloat();
+    this.velX = in.readFloat();
+    this.velY = in.readFloat();
+    this.velZ = in.readFloat();
+  }
+
+  // Getters for the Client Dispatcher
+  public long getEntityId() {
+    return entityId;
+  }
+
+  public BlockType getBlockType() {
+    return BlockRegistry.get(blockTypeId);
+  }
+
+  public float getX() {
+    return x;
+  }
+
+  public float getY() {
+    return y;
+  }
+
+  public float getZ() {
+    return z;
+  }
+
+  public float getVelX() {
+    return velX;
+  }
+
+  public float getVelY() {
+    return velY;
+  }
+
+  public float getVelZ() {
+    return velZ;
+  }
 }
