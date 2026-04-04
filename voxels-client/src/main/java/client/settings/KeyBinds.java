85f224004e56574df2012c386b910a5f8245f3f3
Simon Dietz <h.s.dietz@web.de>
2026-04-04 14:28:42 +0200

    format

--- voxels-client/src/main/java/client/settings/KeyBinds.java
+++ voxels-client/src/main/java/client/settings/KeyBinds.java
@@ -5,32 +5,50 @@ import engine.runtime.input.Key;
 public class KeyBinds {
 
   public static Key strafeLeft = Key.A;
+
   public static Key strafeRight = Key.D;
+
   public static Key walkBackwards = Key.S;
+
   public static Key walkForwards = Key.W;
-  
+
   public static Key dropItem = Key.Q;
-  
+
   public static Key sprint = Key.CTRL;
 
+  // ------------------------------------------
+  // HOTBAR
+  // ------------------------------------------
+
   public static Key hotbarSlot1 = Key.NUM_1;
+
   public static Key hotbarSlot2 = Key.NUM_2;
+
   public static Key hotbarSlot3 = Key.NUM_3;
+
   public static Key hotbarSlot4 = Key.NUM_4;
+
   public static Key hotbarSlot5 = Key.NUM_5;
+
   public static Key hotbarSlot6 = Key.NUM_6;
+
   public static Key hotbarSlot7 = Key.NUM_7;
+
   public static Key hotbarSlot8 = Key.NUM_8;
+
   public static Key hotbarSlot9 = Key.NUM_9;
-  
+
   public static Key openCloseInventory = Key.E;
+
   public static Key openChat = Key.T;
-  
+
   // ------------------------------------------
   // DEBUG
   // ------------------------------------------
-  
+
   public static Key showHideChunkBorders = Key.G;
+
   public static Key enableDisableFrustumCulling = Key.F;
+
   public static Key enableDisableChunkBounds = Key.M;
 }
