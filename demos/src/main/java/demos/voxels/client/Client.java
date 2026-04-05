5d0cac2b944d492b0efb885e84fcc16a9559b74f
Simon Dietz <h.s.dietz@web.de>
2026-04-05 13:50:06 +0200

    removed * imports

--- demos/src/main/java/demos/voxels/client/Client.java
+++ demos/src/main/java/demos/voxels/client/Client.java
@@ -1,7 +1,10 @@
 package demos.voxels.client;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import demos.voxels.client.event.Event;
 import demos.voxels.client.event.EventListener;
