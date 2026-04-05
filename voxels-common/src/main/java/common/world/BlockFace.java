024cdaa3ec3f6305982ff7df4faebdcb214a211e
Simon Dietz <h.s.dietz@web.de>
2026-04-05 11:36:32 +0200

    format

--- voxels-common/src/main/java/common/world/BlockFace.java
+++ voxels-common/src/main/java/common/world/BlockFace.java
@@ -2,15 +2,23 @@ package common.world;
 
 public enum BlockFace {
   UP(0, 1, 0),
+
   DOWN(0, -1, 0),
+
   NORTH(0, 0, -1),
+
   SOUTH(0, 0, 1),
+
   WEST(-1, 0, 0),
+
   EAST(1, 0, 0),
+
   NONE(0, 0, 0);
 
   public final int x;
+
   public final int y;
+
   public final int z;
 
   BlockFace(int x, int y, int z) {
