9a3a75f23037cdb50782152b15e0df217adc5dbf
Simon Dietz <h.s.dietz@web.de>
2026-04-05 02:22:47 +0200

    format

--- voxels-server/src/main/java/server/usecases/inventoryclear/InventoryClearMessages.java
+++ voxels-server/src/main/java/server/usecases/inventoryclear/InventoryClearMessages.java
@@ -2,9 +2,9 @@ package server.usecases.inventoryclear;
 
 public class InventoryClearMessages {
 
+  public static final String INVENTORY_CLEARED = "Inventory cleared.";
+
   private InventoryClearMessages() {
     // No instances
   }
-
-  public static final String INVENTORY_CLEARED = "Inventory cleared.";
 }
