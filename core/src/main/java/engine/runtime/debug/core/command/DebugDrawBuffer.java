d20f1d88095eb0af6f577c0bd2f53e17dde42e45
Simon Dietz <h.s.dietz@web.de>
2026-04-06 20:42:36 +0200

    TODOs for empty methods

--- core/src/main/java/engine/runtime/debug/core/command/DebugDrawBuffer.java
+++ core/src/main/java/engine/runtime/debug/core/command/DebugDrawBuffer.java
@@ -5,12 +5,18 @@ import java.util.List;
 import engine.rendering.Graphics;
 
 class DebugDrawBuffer {
-    
+
   List<DebugCommand> commands;
 
-  void add(DebugCommand cmd) {}
+  void add(DebugCommand cmd) {
+    // TODO implement
+  }
 
-  void update(float tpf) {}
+  void update(float tpf) {
+    // TODO implement
+  }
 
-  void render(Graphics g) {}
+  void render(Graphics g) {
+    // TODO implement
+  }
 }
