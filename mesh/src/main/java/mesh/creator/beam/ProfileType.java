9bdae8477bd03a43b39b76b640680a43b7da36ea
Simon Dietz <h.s.dietz@web.de>
2026-04-10 21:44:35 +0200

    format

--- mesh/src/main/java/mesh/creator/beam/ProfileType.java
+++ mesh/src/main/java/mesh/creator/beam/ProfileType.java
@@ -1,17 +1,15 @@
 package mesh.creator.beam;
 
 public enum ProfileType {
+  O_PROFILE,
 
-    O_PROFILE, 
-    
-    U_PROFILE, 
-    
-    C_PROFILE, 
-    
-    L_PROFILE, 
-    
-    I_PROFILE, 
-    
-    T_PROFILE;
+  U_PROFILE,
 
+  C_PROFILE,
+
+  L_PROFILE,
+
+  I_PROFILE,
+
+  T_PROFILE;
 }
