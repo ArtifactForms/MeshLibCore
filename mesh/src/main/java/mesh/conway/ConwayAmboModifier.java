16c80ad6bf09a558c1818a6e6ae4ba922e07b322
Simon Dietz <h.s.dietz@web.de>
2026-04-11 21:30:53 +0200

    format

--- mesh/src/main/java/mesh/conway/ConwayAmboModifier.java
+++ mesh/src/main/java/mesh/conway/ConwayAmboModifier.java
@@ -13,163 +13,159 @@ import mesh.util.TraverseHelper;
 
 public class ConwayAmboModifier implements IMeshModifier {
 
-    private Mesh3D mesh;
+  private Mesh3D mesh;
 
-    private ArrayList<Face3D> facesToAdd;
+  private ArrayList<Face3D> facesToAdd;
 
-    private List<Vector3f> verticesToAdd;
+  private List<Vector3f> verticesToAdd;
 
-    @Override
-    public Mesh3D modify(Mesh3D mesh) {
-        setMesh(mesh);
-        initializeLists();
-        processFaces();
-        createFacesFromAdjacentVertices();
-        clearOriginalGeometry();
-        addNewlyCreatedGeometry();
-        return mesh;
+  @Override
+  public Mesh3D modify(Mesh3D mesh) {
+    setMesh(mesh);
+    initializeLists();
+    processFaces();
+    createFacesFromAdjacentVertices();
+    clearOriginalGeometry();
+    addNewlyCreatedGeometry();
+    return mesh;
+  }
+
+  private void processFaces() {
+    for (Face3D face : mesh.getFaces()) {
+      addNewFace(createEdgePoints(face));
+    }
+  }
+
+  private int[] createEdgePoints(Face3D face) {
+    int nextIndex = getNextVertexIndex();
+    int[] edgePointIndices = new int[face.indices.length];
+
+    for (int i = 0; i < face.indices.length; i++) {
+      Vector3f edgePoint = calculateEdgePointForEdgeAt(face, i);
+      int edgePointIndex = findIndexOfNewVertex(edgePoint);
+
+      if (edgePointIndex > -1) {
+        edgePointIndices[i] = edgePointIndex;
+      } else {
+        addNewVertex(edgePoint);
+        edgePointIndices[i] = nextIndex;
+        nextIndex++;
+      }
     }
 
-    private void processFaces() {
-        for (Face3D face : mesh.getFaces()) {
-            addNewFace(createEdgePoints(face));
-        }
+    return edgePointIndices;
+  }
+
+  /** Calculate the edge point (mid point) of the edge at the specified index. */
+  private Vector3f calculateEdgePointForEdgeAt(Face3D face, int edgeIndex) {
+    int faceVertexCount = face.indices.length;
+    int fromIndex = face.indices[edgeIndex % faceVertexCount];
+    int toIndex = face.indices[(edgeIndex + 1) % faceVertexCount];
+    return calculateEdgePoint(fromIndex, toIndex);
+  }
+
+  /**
+   * For each vertex of the original mesh, connect the new points that have been generated for the
+   * faces that are adjacent to this vertex.
+   */
+  private void createFacesFromAdjacentVertices() {
+    TraverseHelper helper = new TraverseHelper(mesh);
+    for (int i = 0; i < mesh.getVertexCount(); i++) {
+      Edge3D outgoingEdge = helper.getOutgoing(i);
+      Edge3D edge = outgoingEdge;
+      Vector<Integer> indices = new Vector<Integer>();
+      do {
+        Vector3f from = getOriginalVertexAt(edge.getFromIndex());
+        Vector3f to = getOriginalVertexAt(edge.getToIndex());
+        Vector3f edgePoint = calculateEdgePoint(from, to);
+        int index = verticesToAdd.indexOf(edgePoint);
+        indices.add(index);
+        edge = helper.getPairNext(edge.getFromIndex(), edge.getToIndex());
+      } while (!outgoingEdge.equals(edge));
+      Face3D face = new Face3D(toReverseArray(indices));
+      face.setTag("ambo");
+      facesToAdd.add(face);
     }
+  }
 
-    private int[] createEdgePoints(Face3D face) {
-        int nextIndex = getNextVertexIndex();
-        int[] edgePointIndices = new int[face.indices.length];
-
-        for (int i = 0; i < face.indices.length; i++) {
-            Vector3f edgePoint = calculateEdgePointForEdgeAt(face, i);
-            int edgePointIndex = findIndexOfNewVertex(edgePoint);
-
-            if (edgePointIndex > -1) {
-                edgePointIndices[i] = edgePointIndex;
-            } else {
-                addNewVertex(edgePoint);
-                edgePointIndices[i] = nextIndex;
-                nextIndex++;
-            }
-        }
-
-        return edgePointIndices;
+  private int[] toReverseArray(Vector<Integer> values) {
+    int[] a = new int[values.size()];
+    for (int j = 0; j < a.length; j++) {
+      int index = a.length - j - 1;
+      a[index] = values.get(j);
     }
+    return a;
+  }
 
-    /**
-     * Calculate the edge point (mid point) of the edge at the specified index.
-     */
-    private Vector3f calculateEdgePointForEdgeAt(Face3D face, int edgeIndex) {
-        int faceVertexCount = face.indices.length;
-        int fromIndex = face.indices[edgeIndex % faceVertexCount];
-        int toIndex = face.indices[(edgeIndex + 1) % faceVertexCount];
-        return calculateEdgePoint(fromIndex, toIndex);
-    }
+  private void initializeLists() {
+    initializeFaceList();
+    initializeVertexList();
+  }
 
-    /**
-     * For each vertex of the original mesh, connect the new points that have
-     * been generated for the faces that are adjacent to this vertex.
-     */
-    private void createFacesFromAdjacentVertices() {
-        TraverseHelper helper = new TraverseHelper(mesh);
-        for (int i = 0; i < mesh.getVertexCount(); i++) {
-            Edge3D outgoingEdge = helper.getOutgoing(i);
-            Edge3D edge = outgoingEdge;
-            Vector<Integer> indices = new Vector<Integer>();
-            do {
-                Vector3f from = getOriginalVertexAt(edge.getFromIndex());
-                Vector3f to = getOriginalVertexAt(edge.getToIndex());
-                Vector3f edgePoint = calculateEdgePoint(from, to);
-                int index = verticesToAdd.indexOf(edgePoint);
-                indices.add(index);
-                edge = helper.getPairNext(edge.getFromIndex(), edge.getToIndex());
-            } while (!outgoingEdge.equals(edge));
-            Face3D face = new Face3D(toReverseArray(indices));
-            face.setTag("ambo");
-            facesToAdd.add(face);
-        }
-    }
+  private void clearOriginalGeometry() {
+    removeOriginalVerices();
+    removeOriginalFaces();
+  }
 
-    private int[] toReverseArray(Vector<Integer> values) {
-        int[] a = new int[values.size()];
-        for (int j = 0; j < a.length; j++) {
-            int index = a.length - j - 1;
-            a[index] = values.get(j);
-        }
-        return a;
-    }
+  private void addNewlyCreatedGeometry() {
+    addNewlyVertices();
+    addNewlyCreatedFaces();
+  }
 
-    private void initializeLists() {
-        initializeFaceList();
-        initializeVertexList();
-    }
+  private Vector3f getOriginalVertexAt(int index) {
+    return mesh.getVertexAt(index);
+  }
 
-    private void clearOriginalGeometry() {
-        removeOriginalVerices();
-        removeOriginalFaces();
-    }
+  private void removeOriginalVerices() {
+    mesh.clearVertices();
+  }
 
-    private void addNewlyCreatedGeometry() {
-        addNewlyVertices();
-        addNewlyCreatedFaces();
-    }
+  private void addNewlyVertices() {
+    mesh.addVertices(verticesToAdd);
+  }
 
-    private Vector3f getOriginalVertexAt(int index) {
-        return mesh.getVertexAt(index);
-    }
+  private void initializeFaceList() {
+    facesToAdd = new ArrayList<>();
+  }
 
-    private void removeOriginalVerices() {
-        mesh.clearVertices();
-    }
+  private void initializeVertexList() {
+    verticesToAdd = new ArrayList<Vector3f>();
+  }
 
-    private void addNewlyVertices() {
-        mesh.addVertices(verticesToAdd);
-    }
+  private int getNextVertexIndex() {
+    return verticesToAdd.size();
+  }
 
-    private void initializeFaceList() {
-        facesToAdd = new ArrayList<>();
-    }
+  private void addNewVertex(Vector3f v) {
+    verticesToAdd.add(v);
+  }
 
-    private void initializeVertexList() {
-        verticesToAdd = new ArrayList<Vector3f>();
-    }
+  private int findIndexOfNewVertex(Vector3f v) {
+    return verticesToAdd.indexOf(v);
+  }
 
-    private int getNextVertexIndex() {
-        return verticesToAdd.size();
-    }
+  private void addNewlyCreatedFaces() {
+    mesh.addFaces(facesToAdd);
+  }
 
-    private void addNewVertex(Vector3f v) {
-        verticesToAdd.add(v);
-    }
+  private void removeOriginalFaces() {
+    mesh.clearFaces();
+  }
 
-    private int findIndexOfNewVertex(Vector3f v) {
-        return verticesToAdd.indexOf(v);
-    }
+  private void addNewFace(int[] indices) {
+    facesToAdd.add(new Face3D(indices));
+  }
 
-    private void addNewlyCreatedFaces() {
-        mesh.addFaces(facesToAdd);
-    }
+  private Vector3f calculateEdgePoint(int startVertexIndex, int endVertexIndex) {
+    return calculateEdgePoint(
+        getOriginalVertexAt(startVertexIndex), getOriginalVertexAt(endVertexIndex));
+  }
 
-    private void removeOriginalFaces() {
-        mesh.clearFaces();
-    }
+  private Vector3f calculateEdgePoint(Vector3f start, Vector3f end) {
+    return start.add(end).mult(0.5f);
+  }
 
-    private void addNewFace(int[] indices) {
-        facesToAdd.add(new Face3D(indices));
-    }
-
-    private Vector3f calculateEdgePoint(int startVertexIndex,
-            int endVertexIndex) {
-        return calculateEdgePoint(getOriginalVertexAt(startVertexIndex),
-                getOriginalVertexAt(endVertexIndex));
-    }
-
-    private Vector3f calculateEdgePoint(Vector3f start, Vector3f end) {
-        return start.add(end).mult(0.5f);
-    }
-
-    private void setMesh(Mesh3D mesh) {
-        this.mesh = mesh;
-    }
-
+  private void setMesh(Mesh3D mesh) {
+    this.mesh = mesh;
+  }
 }
--- mesh/src/main/java/mesh/conway/RandomConwayCreator.java
+++ mesh/src/main/java/mesh/conway/RandomConwayCreator.java
@@ -8,70 +8,68 @@ import mesh.modifier.transform.FitToAABBModifier;
 
 public class RandomConwayCreator implements IMeshCreator {
 
-    private int expressionLength;
+  private int expressionLength;
 
-    private boolean fitToBounds;
+  private boolean fitToBounds;
 
-    private String lastExpression;
+  private String lastExpression;
 
-    public RandomConwayCreator() {
-        fitToBounds = true;
-        expressionLength = 5;
+  public RandomConwayCreator() {
+    fitToBounds = true;
+    expressionLength = 5;
+  }
+
+  @Override
+  public Mesh3D create() {
+    String expression = createExpression();
+    setLastExpression(expression);
+
+    Mesh3D seed = new CubeCreator().create();
+    Conway conway = new Conway();
+    conway.create(seed, expression);
+
+    if (fitToBounds) new FitToAABBModifier(2, 2, 2).modify(seed);
+
+    return seed;
+  }
+
+  private String createExpression() {
+    String expression = "";
+
+    for (int i = 0; i < expressionLength; i++) {
+      int random = Mathf.random(0, 3);
+
+      switch (random) {
+        case 0:
+          expression += "k";
+          break;
+        case 1:
+          expression += "a";
+          break;
+        case 2:
+          expression += "d";
+          break;
+        default:
+          break;
+      }
     }
 
-    @Override
-    public Mesh3D create() {
-        String expression = createExpression();
-        setLastExpression(expression);
+    return expression;
+  }
 
-        Mesh3D seed = new CubeCreator().create();
-        Conway conway = new Conway();
-        conway.create(seed, expression);
+  private void setLastExpression(String lastExpression) {
+    this.lastExpression = lastExpression;
+  }
 
-        if (fitToBounds)
-            new FitToAABBModifier(2, 2, 2).modify(seed);
+  public String getLastExpression() {
+    return lastExpression;
+  }
 
-        return seed;
-    }
+  public int getN() {
+    return expressionLength;
+  }
 
-    private String createExpression() {
-        String expression = "";
-
-        for (int i = 0; i < expressionLength; i++) {
-            int random = Mathf.random(0, 3);
-
-            switch (random) {
-            case 0:
-                expression += "k";
-                break;
-            case 1:
-                expression += "a";
-                break;
-            case 2:
-                expression += "d";
-                break;
-            default:
-                break;
-            }
-        }
-
-        return expression;
-    }
-
-    private void setLastExpression(String lastExpression) {
-        this.lastExpression = lastExpression;
-    }
-
-    public String getLastExpression() {
-        return lastExpression;
-    }
-
-    public int getN() {
-        return expressionLength;
-    }
-
-    public void setN(int n) {
-        this.expressionLength = n;
-    }
-
+  public void setN(int n) {
+    this.expressionLength = n;
+  }
 }
