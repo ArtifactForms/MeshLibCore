083d5817c433ae56cc06af4e25080756786d4cf5
Simon Dietz <h.s.dietz@web.de>
2026-04-11 09:44:14 +0200

    cleaned imports - removed * imports

--- demos/src/main/java/demos/voxels/server/Server.java
+++ demos/src/main/java/demos/voxels/server/Server.java
@@ -1,8 +1,13 @@
 package demos.voxels.server;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
 
