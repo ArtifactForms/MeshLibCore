e86809ada876a45dba2fbbf923c094c816590a80
Simon Dietz <h.s.dietz@web.de>
2026-04-12 11:11:13 +0200

    format

--- voxels-server/src/main/java/server/gateways/GatewayContext.java
+++ voxels-server/src/main/java/server/gateways/GatewayContext.java
@@ -1,13 +1,12 @@
 package server.gateways;
 
 public record GatewayContext(
-	    WorldGateway world,
-	    EventGateway events,
-	    PermissionGateway permissions,
-	    InventoryGateway inventory,
-	    ConfigGateway config,
-	    CommandGateway commands,
-	    PlayerGateway players,
-	    MessageGateway messages,
-	    ServerGateway server
-	) {}
+  WorldGateway world,
+  EventGateway events,
+  PermissionGateway permissions,
+  InventoryGateway inventory,
+  ConfigGateway config,
+  CommandGateway commands,
+  PlayerGateway players,
+  MessageGateway messages,
+  ServerGateway servery) {}
