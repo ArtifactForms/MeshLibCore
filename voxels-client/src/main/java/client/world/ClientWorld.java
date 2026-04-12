8a19fa889db3eec32b7381926e020969ed9c310d
Simon Dietz <h.s.dietz@web.de>
2026-04-12 20:36:17 +0200

    removed clutter

--- voxels-client/src/main/java/client/world/ClientWorld.java
+++ voxels-client/src/main/java/client/world/ClientWorld.java
@@ -11,15 +11,12 @@ public class ClientWorld extends World {
   private float fractionalTickCounter = 0;
 
   private ChunkManager chunkManager;
-  
-  // Warteschlange für Datenpakete vom Server
+
   private final ConcurrentLinkedQueue<ChunkDataPacket> incomingPackets =
       new ConcurrentLinkedQueue<>();
 
-  // Container für die Netzwerk-Daten
   private record ChunkDataPacket(int cx, int cz, short[] blocks) {}
 
-  /** Wird vom Netzwerk-Thread aufgerufen, wenn ein Chunk-Paket ankommt. */
   public void applyChunkData(int cx, int cz, short[] blocks) {
     incomingPackets.offer(new ChunkDataPacket(cx, cz, blocks));
   }
@@ -34,13 +31,10 @@ public class ClientWorld extends World {
       if (packet == null) continue;
 
       Chunk chunk = chunkManager.getOrCreateChunk(packet.cx, packet.cz);
-
-      // Referenz-Zuweisung statt Kopie (Extrem schnell!)
       chunk.setBlockData(packet.blocks);
-
       chunk.setDataReady();
       chunk.markDirty();
-      addChunk(chunk); // Wichtig für Raycasts/Interaktion!
+      addChunk(chunk);
 
       chunkManager.notifyNeighborsOfDataReady(packet.cx, packet.cz);
     }
@@ -56,22 +50,12 @@ public class ClientWorld extends World {
   }
 
   public void update(float tpf) {
-    // Wir addieren den Fortschritt auf einen float-Counter
     fractionalTickCounter += tpf * TICKS_PER_SECOND;
 
-    // Sobald der Counter >= 1 ist, schieben wir die vollen Ticks in worldTime
     if (fractionalTickCounter >= 1.0f) {
       int fullTicks = (int) fractionalTickCounter;
-//      worldTime += fullTicks;
       tick();
       fractionalTickCounter -= fullTicks;
     }
   }
-
-//  @Override
-//  public float getTimeOfDay() {
-//    // worldTime (ganze Ticks) + fractionalTickCounter (Nachkommastellen)
-//    float preciseTime = worldTime + fractionalTickCounter;
-//    return (preciseTime % WorldTime.DAY_LENGTH) / (float) WorldTime.DAY_LENGTH;
-//  }
 }
