daa4d94378779207ed0d0ebf129d66c8b5cbc186
Simon Dietz <h.s.dietz@web.de>
2026-04-12 11:44:53 +0200

    removed magic number added constructor with scale parameter

--- mesh/src/main/java/mesh/creator/catalan/TriakisTetrahedronCreator.java
+++ mesh/src/main/java/mesh/creator/catalan/TriakisTetrahedronCreator.java
@@ -5,62 +5,75 @@ import mesh.creator.IMeshCreator;
 
 public class TriakisTetrahedronCreator implements IMeshCreator {
 
-    private float a = 5f / 3f;
+  /**
+   * Scale factor for tetrahedron vertices so that the resulting triakis tetrahedron has symmetric
+   * (congruent) faces.
+   */
+  private static final float DEFAULT_TRIAKIS_TETRAHEDRON_SCALE = 5f / 3f;
 
-    private Mesh3D mesh;
+  private float scale;
 
-    @Override
-    public Mesh3D create() {
-        initializeMesh();
-        createVertices();
-        createFaces();
-        return mesh;
-    }
+  private Mesh3D mesh;
 
-    private void createVertices() {
-        createTetrahedronVertices();
-        createMidEdgeVertices();
-    }
+  public TriakisTetrahedronCreator() {
+    this(DEFAULT_TRIAKIS_TETRAHEDRON_SCALE);
+  }
 
-    private void createTetrahedronVertices() {
-        addVertex(a, a, a);
-        addVertex(a, -a, -a);
-        addVertex(-a, -a, a);
-        addVertex(-a, a, -a);
-    }
+  public TriakisTetrahedronCreator(float scale) {
+    this.scale = scale;
+  }
 
-    private void createMidEdgeVertices() {
-        addVertex(1, -1, 1);
-        addVertex(-1, 1, 1);
-        addVertex(1, 1, -1);
-        addVertex(-1, -1, -1);
-    }
+  @Override
+  public Mesh3D create() {
+    initializeMesh();
+    createVertices();
+    createFaces();
+    return mesh;
+  }
 
-    public void createFaces() {
-        addFace(4, 0, 5);
-        addFace(6, 1, 7);
-        addFace(2, 4, 5);
-        addFace(3, 6, 7);
-        addFace(5, 0, 6);
-        addFace(1, 4, 7);
-        addFace(4, 2, 7);
-        addFace(3, 5, 6);
-        addFace(0, 4, 6);
-        addFace(4, 1, 6);
-        addFace(2, 5, 7);
-        addFace(5, 3, 7);
-    }
+  private void createVertices() {
+    createTetrahedronVertices();
+    createInnerVertices();
+  }
 
-    private void initializeMesh() {
-        mesh = new Mesh3D();
-    }
+  private void createTetrahedronVertices() {
+    addVertex(scale, scale, scale);
+    addVertex(scale, -scale, -scale);
+    addVertex(-scale, -scale, scale);
+    addVertex(-scale, scale, -scale);
+  }
 
-    private void addVertex(float x, float y, float z) {
-        mesh.addVertex(x, y, z);
-    }
+  private void createInnerVertices() {
+    addVertex(1, -1, 1);
+    addVertex(-1, 1, 1);
+    addVertex(1, 1, -1);
+    addVertex(-1, -1, -1);
+  }
 
-    private void addFace(int... indices) {
-        mesh.addFace(indices);
-    }
+  public void createFaces() {
+    addFace(4, 0, 5);
+    addFace(6, 1, 7);
+    addFace(2, 4, 5);
+    addFace(3, 6, 7);
+    addFace(5, 0, 6);
+    addFace(1, 4, 7);
+    addFace(4, 2, 7);
+    addFace(3, 5, 6);
+    addFace(0, 4, 6);
+    addFace(4, 1, 6);
+    addFace(2, 5, 7);
+    addFace(5, 3, 7);
+  }
 
+  private void initializeMesh() {
+    mesh = new Mesh3D();
+  }
+
+  private void addVertex(float x, float y, float z) {
+    mesh.addVertex(x, y, z);
+  }
+
+  private void addFace(int... indices) {
+    mesh.addFace(indices);
+  }
 }
