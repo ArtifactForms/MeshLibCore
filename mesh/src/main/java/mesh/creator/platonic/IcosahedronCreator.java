c1a330e822a5388225f181c1fb9ceb2d8c14b149
Simon Dietz <h.s.dietz@web.de>
2026-04-19 12:36:11 +0200

    format

--- mesh/src/main/java/mesh/creator/platonic/IcosahedronCreator.java
+++ mesh/src/main/java/mesh/creator/platonic/IcosahedronCreator.java
@@ -6,115 +6,114 @@ import mesh.modifier.transform.ScaleModifier;
 
 public class IcosahedronCreator implements IMeshCreator {
 
-    private float a = 0.723600f;
+  private float a = 0.723600f;
 
-    private float b = 0.447215f;
+  private float b = 0.447215f;
 
-    private float c = 0.850640f;
+  private float c = 0.850640f;
 
-    private float d = 0.525720f;
+  private float d = 0.525720f;
 
-    private float e = 0.276385f;
+  private float e = 0.276385f;
 
-    private float f = 0.894425f;
+  private float f = 0.894425f;
 
-    private float size;
+  private float size;
 
-    private Mesh3D mesh;
+  private Mesh3D mesh;
 
-    public IcosahedronCreator() {
-        this(1);
-    }
+  public IcosahedronCreator() {
+    this(1);
+  }
 
-    public IcosahedronCreator(float size) {
-        this.size = size;
-    }
+  public IcosahedronCreator(float size) {
+    this.size = size;
+  }
 
-    @Override
-    public Mesh3D create() {
-        initializeMesh();
-        createVertices();
-        createFaces();
-        scaleMesh();
-        return mesh;
-    }
+  @Override
+  public Mesh3D create() {
+    initializeMesh();
+    createVertices();
+    createFaces();
+    scaleMesh();
+    return mesh;
+  }
 
-    private void initializeMesh() {
-        mesh = new Mesh3D();
-    }
+  private void initializeMesh() {
+    mesh = new Mesh3D();
+  }
 
-    private void createVertices() {
-        addVertex(0, -1, 0);
-        addVertex(a, -b, d);
-        addVertex(-e, -b, c);
-        addVertex(-f, -b, 0);
-        addVertex(-e, -b, -c);
-        addVertex(a, -b, -d);
-        addVertex(e, b, c);
-        addVertex(-a, b, d);
-        addVertex(-a, b, -d);
-        addVertex(e, b, -c);
-        addVertex(f, b, 0);
-        addVertex(0, 1, 0);
-    }
+  private void createVertices() {
+    addVertex(0, -1, 0);
+    addVertex(a, -b, d);
+    addVertex(-e, -b, c);
+    addVertex(-f, -b, 0);
+    addVertex(-e, -b, -c);
+    addVertex(a, -b, -d);
+    addVertex(e, b, c);
+    addVertex(-a, b, d);
+    addVertex(-a, b, -d);
+    addVertex(e, b, -c);
+    addVertex(f, b, 0);
+    addVertex(0, 1, 0);
+  }
 
-    private void createFaces() {
-        createTopFaces();
-        createSideTopFaces();
-        createSideBottomFaces();
-        createBottomFaces();
-    }
+  private void createFaces() {
+    createTopFaces();
+    createSideTopFaces();
+    createSideBottomFaces();
+    createBottomFaces();
+  }
 
-    private void createSideTopFaces() {
-        addFace(1, 5, 10);
-        addFace(2, 1, 6);
-        addFace(3, 2, 7);
-        addFace(4, 3, 8);
-        addFace(5, 4, 9);
-    }
+  private void createSideTopFaces() {
+    addFace(1, 5, 10);
+    addFace(2, 1, 6);
+    addFace(3, 2, 7);
+    addFace(4, 3, 8);
+    addFace(5, 4, 9);
+  }
 
-    private void createSideBottomFaces() {
-        addFace(10, 6, 1);
-        addFace(6, 7, 2);
-        addFace(7, 8, 3);
-        addFace(8, 9, 4);
-        addFace(9, 10, 5);
-    }
+  private void createSideBottomFaces() {
+    addFace(10, 6, 1);
+    addFace(6, 7, 2);
+    addFace(7, 8, 3);
+    addFace(8, 9, 4);
+    addFace(9, 10, 5);
+  }
 
-    private void createTopFaces() {
-        addFace(2, 0, 1);
-        addFace(1, 0, 5);
-        addFace(3, 0, 2);
-        addFace(4, 0, 3);
-        addFace(5, 0, 4);
-    }
+  private void createTopFaces() {
+    addFace(2, 0, 1);
+    addFace(1, 0, 5);
+    addFace(3, 0, 2);
+    addFace(4, 0, 3);
+    addFace(5, 0, 4);
+  }
 
-    private void createBottomFaces() {
-        addFace(6, 10, 11);
-        addFace(7, 6, 11);
-        addFace(8, 7, 11);
-        addFace(9, 8, 11);
-        addFace(10, 9, 11);
-    }
+  private void createBottomFaces() {
+    addFace(6, 10, 11);
+    addFace(7, 6, 11);
+    addFace(8, 7, 11);
+    addFace(9, 8, 11);
+    addFace(10, 9, 11);
+  }
 
-    private void addFace(int... vertices) {
-        mesh.addFace(vertices);
-    }
+  private void addFace(int... vertices) {
+    mesh.addFace(vertices);
+  }
 
-    private void addVertex(float x, float y, float z) {
-        mesh.addVertex(x, y, z);
-    }
+  private void addVertex(float x, float y, float z) {
+    mesh.addVertex(x, y, z);
+  }
 
-    private void scaleMesh() {
-        new ScaleModifier(size).modify(mesh);
-    }
+  private void scaleMesh() {
+    new ScaleModifier(size).modify(mesh);
+  }
 
-    public float getSize() {
-        return size;
-    }
+  public float getSize() {
+    return size;
+  }
 
-    public void setSize(float size) {
-        this.size = size;
-    }
-
+  public void setSize(float size) {
+    this.size = size;
+  }
 }
--- mesh/src/main/java/mesh/creator/platonic/PlatonicSolid.java
+++ mesh/src/main/java/mesh/creator/platonic/PlatonicSolid.java
@@ -1,47 +1,44 @@
 package mesh.creator.platonic;
 
 public enum PlatonicSolid {
+  TETRAHEDRON(4, 4, 6, "TETRAHEDRON"),
 
-    TETRAHEDRON(4, 4, 6, "TETRAHEDRON"),
+  HEXAHEDRON(6, 8, 12, "HEXAHEDRON"),
 
-    HEXAHEDRON(6, 8, 12, "HEXAHEDRON"),
+  OCTAHEDRON(8, 6, 12, "OCTAHEDRON"),
 
-    OCTAHEDRON(8, 6, 12, "OCTAHEDRON"),
+  ICOSAHEDRON(20, 12, 30, "ICOSAHEDRON"),
 
-    ICOSAHEDRON(20, 12, 30, "ICOSAHEDRON"),
+  DODECAHEDRON(12, 20, 30, "DODECAHEDRON");
 
-    DODECAHEDRON(12, 20, 30, "DODECAHEDRON");
+  private int faceCount;
 
-    private int faceCount;
+  private int vertexCount;
 
-    private int vertexCount;
+  private int edgeCount;
 
-    private int edgeCount;
+  private String name;
 
-    private String name;
+  private PlatonicSolid(int faceCount, int vertexCount, int edgeCount, String name) {
+    this.faceCount = faceCount;
+    this.vertexCount = vertexCount;
+    this.edgeCount = edgeCount;
+    this.name = name;
+  }
 
-    private PlatonicSolid(int faceCount, int vertexCount, int edgeCount,
-            String name) {
-        this.faceCount = faceCount;
-        this.vertexCount = vertexCount;
-        this.edgeCount = edgeCount;
-        this.name = name;
-    }
+  public int getFaceCount() {
+    return faceCount;
+  }
 
-    public int getFaceCount() {
-        return faceCount;
-    }
+  public int getVertexCount() {
+    return vertexCount;
+  }
 
-    public int getVertexCount() {
-        return vertexCount;
-    }
+  public int getEdgeCount() {
+    return edgeCount;
+  }
 
-    public int getEdgeCount() {
-        return edgeCount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
+  public String getName() {
+    return name;
+  }
 }
